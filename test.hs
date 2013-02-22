fd95372770fbcda51387748638bcc4728f61a92f
Hans Höglund <hans@hanshoglund.se>
2013-02-22 21:22:15 +0100

    Testing

--- test.hs
+++ test.hs
@@ -4,36 +4,55 @@ module Main where
 import Data.Semigroup
 import Data.Default
 import Music.MusicXml
+import System.Posix.Process
 
 -- division 38880 (2^5*3^5*5)
 
 score = Partwise
     (ScoreAttrs [1])
-    (ScoreHeader [])
+    (ScoreHeader 
+        (Just "Frère Jaques") 
+        (Just "I") 
+        (Just (Identification [Creator "Music" "Anonymous"])) 
+        [
+            Part "1" "Voice" Nothing
+        ])
     [
         -- part 1
         [
             -- measure 1
-            (MeasureAttrs 1, [ 
-                MusicAttr (Div 256)
+            (MeasureAttrs 1, [                
+                -- setting attributes as this is first measure 
+                MusicAttributes (Div 256)
                 ,
-                MusicAttr (Clef GClef 2)
+                MusicAttributes (Clef GClef 2)
                 ,
-                MusicAttr (Key (-3))
+                MusicAttributes (Key (-3) Major)
                 ,
-                MusicAttr (Time CommonTime)
+                MusicAttributes (Time CommonTime)
                 ,
-                
-            
-                MusicNote (Note (Pitched True (60,Nothing,0)) (Divisions 256) [])
+                            
+                MusicNote (Note (Pitched chord (60, noSemitones, 0)) 256 noTies)
                 , 
-                MusicNote (Note (Pitched True (62,Nothing,0)) (Divisions 256) [])
+                MusicNote (Note (Pitched chord (62, noSemitones, 0)) 256 noTies)
                 , 
-                MusicNote (Note (Pitched True (63,Nothing,0)) (Divisions 128) [])
+                MusicNote (Note (Pitched chord (63, noSemitones, 0)) 128 noTies)
                 , 
-                MusicNote (Note (Pitched True (62,Nothing,0)) (Divisions 128) [])
+                MusicNote (Note (Pitched chord (62, noSemitones, 0)) 128 noTies)
                 , 
-                MusicNote (Note (Pitched True (60,Nothing,0)) (Divisions 256) [])
+                MusicNote (Note (Pitched chord (60, noSemitones, 0)) 256 noTies)
+                ])
+            ,
+            (MeasureAttrs 2, [                      
+                MusicNote (Note (Pitched chord (60, noSemitones, 0)) 256 noTies)
+                , 
+                MusicNote (Note (Pitched chord (62, noSemitones, 0)) 256 noTies)
+                , 
+                MusicNote (Note (Pitched chord (63, noSemitones, 0)) 128 noTies)
+                , 
+                MusicNote (Note (Pitched chord (62, noSemitones, 0)) 128 noTies)
+                , 
+                MusicNote (Note (Pitched chord (60, noSemitones, 0)) 256 noTies)
                 ])
         ]
     ]
@@ -42,9 +61,16 @@ score = Partwise
 
 
 
-main = putStrLn $ showXml $ score
+showScore = putStrLn $ showXml $ score
+main = showScore
 
 openXml :: Score -> IO ()
-openXml = undefined -- open in Sibelius
+openXml score = 
+    do  writeFile "test.xml" (showXml score)
+        execute "open" ["-a", "/Applications/Sibelius/Sibelius.app", "test.xml"]
 
 
+execute :: FilePath -> [String] -> IO ()
+execute program args = do
+    forkProcess $ executeFile program True args Nothing
+    return ()
