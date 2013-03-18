f8eeb0fe1ef58f633be8dabf0b08bb5a2f64caa9
Hans Höglund <hans@hanshoglund.se>
2013-03-18 20:54:04 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# music-pitch
+
+Abstract representation of musical pitch.
+
+## Requirements
+
+* [Haskell Platform](http://www.haskell.org/platform)
+
+## Installation
+
+    cabal configure
+    cabal install
---
+++ src/Music/Pitch/Intonation.hs
@@ -0,0 +1,25 @@
+
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Provides overloaded pitch literals.
+--
+-------------------------------------------------------------------------------------
+
+module Music.Pitch.Intonation -- (
+-- )
+where
+
+import Data.Maybe
+import Data.Either
+import Data.Semigroup
+import Control.Monad
+import Control.Applicative
+
