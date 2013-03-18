c187f4d10044b3e8cd1e1a1daba5b9eca8683733
Hans Höglund <hans@hanshoglund.se>
2013-03-18 20:57:58 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# music-articulation
+
+Abstract representation of musical articulation (staccato, legato etc).
+
+## Requirements
+
+* [Haskell Platform](http://www.haskell.org/platform)
+
+## Installation
+
+    cabal configure
+    cabal install
---
+++ src/Music/Articulation.hs
@@ -0,0 +1,25 @@
+
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Provides abstract musical articulations.
+--
+-------------------------------------------------------------------------------------
+
+module Music.Articulation -- (
+-- )
+where
+
+import Data.Maybe
+import Data.Either
+import Data.Semigroup
+import Control.Monad
+import Control.Applicative
+
