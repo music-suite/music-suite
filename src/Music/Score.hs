867e0612618fb3c4d397647e2190e340c44d52c3
Hans Höglund <hans@hanshoglund.se>
2013-03-24 16:53:01 +0100

    Quantization engine

--- src/Music/Score.hs
+++ src/Music/Score.hs
@@ -142,6 +142,7 @@ import Text.Parsec.Pos
 
 import System.Posix -- debug
 import System.IO
+import System.IO.Unsafe --debug
 
 import Music.Pitch.Literal
 import Music.Dynamics.Literal
@@ -900,7 +901,7 @@ openXml sc = do
 
 data Rhythm a 
     = RBeat       Duration a                    -- d is divisible by 2
-    | RDotted     (Rhythm a)
+    | RDotted     Int (Rhythm a)
     | RTuplet     Duration (Rhythm a)           -- d is 2/3, 4/5, 4/6, 4/7, 8/9, 8/10, 8/11 ...
     | RInvTuplet  Duration (Rhythm a)           -- d is 3/2,      6/4
     | RSeq        [Rhythm a]                    
@@ -917,7 +918,7 @@ instance VectorSpace (Rhythm a) where
 
 instance HasDuration (Rhythm a) where
     duration (RBeat d _)        = d
-    duration (RDotted a)        = duration a * (3/2)
+    duration (RDotted n a)      = duration a * dotMod n
     duration (RTuplet c a)      = duration a * c
     -- duration (RInvTuplet c a)   = duration a * c
     duration (RSeq as)          = sum (fmap duration as)    
@@ -936,31 +937,32 @@ data RState = RState {
         -- 3/2 for dots
         -- 2/3, 4/5, 4/6, 4/7, 8/9, 8/10, 8/11  for ordinary tuplets
         -- 3/2,      6/4                        for inverted tuplets
-        dotDepth   :: Int,
         tupleDepth :: Int
     }
 
+instance Monoid RState where
+    mempty = RState { timeMod = 1, tupleDepth = 0 }
+    a `mappend` _ = a
+
+dotMod :: Int -> Duration
+dotMod n = dotMods !! (n-1)
 
 -- [3/2, 7/4, 15/8, 31/16 ..]
-dotMod :: [Rational]
-dotMod = zipWith (/) (fmap pred $ drop 2 times2) (drop 1 times2)
+dotMods :: [Duration]
+dotMods = zipWith (/) (fmap pred $ drop 2 times2) (drop 1 times2)
     where
         times2 = iterate (*2) 1
 
-modifyTimeMod :: (Duration -> Duration) -> RState -> RState
-modifyTimeMod f (RState tm dd td) = RState (f tm) dd td
+tupletMods :: [Duration]
+tupletMods = [2/3, 4/5, {-4/6,-} 4/7, 8/9]
 
-modifyDotDepth :: (Int -> Int) -> RState -> RState
-modifyDotDepth f (RState tm dd td) = RState tm (f dd) td
+modifyTimeMod :: (Duration -> Duration) -> RState -> RState
+modifyTimeMod f (RState tm td) = RState (f tm) td
 
 modifyTupleDepth :: (Int -> Int) -> RState -> RState
-modifyTupleDepth f (RState tm dd td) = RState tm dd (f td)
+modifyTupleDepth f (RState tm td) = RState tm (f td)
 
 
-instance Monoid RState where
-    mempty = RState { timeMod = 1, dotDepth = 0, tupleDepth = 0 }
-    a `mappend` _ = a
-    
 type RhythmParser a b = Parsec [(Duration, a)] RState b
 
 quantize' :: RhythmParser a b -> [(Duration, a)] -> Either String b
@@ -990,32 +992,39 @@ rhythm' = mzero
 
 beat :: RhythmParser a (Rhythm a)
 beat = do
-    RState tm _ _ <- getState
+    RState tm _ <- getState
     (^/tm) <$> match (const . isDivisibleBy 2 . (/ tm))
 
 onlyIf :: MonadPlus m => Bool -> m b -> m b
 onlyIf b p = if b then p else mzero
 
 dotted :: RhythmParser a (Rhythm a)
-dotted = do
-    RState _ dd _ <- getState
-    onlyIf (dd < 3) $ do    
-        modifyState $ modifyTimeMod dot
-        modifyState $ modifyDotDepth succ
+dotted = msum . fmap dotted' $ [1..3] -- max 3 dots
 
-        r <- beat <|> dotted
-        -- TODO dotted does not compose... instead we should match against 3/2, 7/4, 15/8, 31/16 etc
-
-        modifyState $ modifyDotDepth pred
-        modifyState $ modifyTimeMod undot
-        return (RDotted r)
-        where
-            dot   d = d*(3/2)
-            undot d = d*(2/3)
-
+dotted' :: Int -> RhythmParser a (Rhythm a)
+dotted' n = do
+    modifyState $ modifyTimeMod (* dotMod n)
+    b <- beat
+    modifyState $ modifyTimeMod (/ dotMod n)
+    return (RDotted n b)
 
 tuplet :: RhythmParser a (Rhythm a)
-tuplet = mzero
+tuplet = msum . fmap tuplet' $ tupletMods
+
+-- tuplet' 2/3 for triplet, 4/5 for quintuplet etc
+tuplet' :: Duration -> RhythmParser a (Rhythm a)
+tuplet' d = do
+    RState _ depth <- getState
+    onlyIf (depth < 1) $ do             -- max 1 nested tuplets
+        modifyState $ id
+            . modifyTimeMod (* d) 
+            . modifyTupleDepth succ
+        r <- rhythm -- TODO rhythm
+        modifyState $ id
+            . modifyTimeMod (/ d) 
+            . modifyTupleDepth pred
+        return (RTuplet d r)
+
 
 
 
