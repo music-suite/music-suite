7ccafa636e6633fd6f85b0f5adf2ec36e1bc1c62
Hans Höglund <hans@hanshoglund.se>
2013-06-06 17:48:01 +0200

    Tests for set and new

--- src/Music/Lilypond.hs
+++ src/Music/Lilypond.hs
@@ -834,37 +834,68 @@ engrave e = do
 
 main = engrave test
 
-test =
-    Simultaneous False 
-        [ Relative g' (Sequential [
-            addMarkup ([Bold "Hello", Italic (markup [MarkupText "cruel", Bold $ MarkupText "world"])]) rest,
-            addArticulation Mordent $ chord [c,e,g]^*2,
-            d^*1,
-            e^*2,
-            c^*(3/2),
-            fs^*(1/2)
-            ])
-        , Sequential [Tremolo 4 (Sequential [c^/4,d^/4]), Tremolo 4 (Sequential [c^/4,d^/4])]
-        , Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)]
-        , Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)]
-        , Relative g (Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)])
-        , Sequential 
-            [ Times (4/5) (Sequential 
-                [
-                    rest,
-                    addArticulation Accent $ addPost BeginSlur $ addPost BeginCresc $ c^*2,
-                    d^*1,
-                    addPost Tie $ e^*1
-                ])
-            , Times (4/5) (Sequential 
-                [      
-                    addPost BeginDim $ addPost EndCrescDim $ e^*1,
-                    c^*(3/2),
-                    addPost EndSlur $ fs^*(1/2),
-                    addPost EndCrescDim $ c^*2
-                ])
-            ]
-        ]
+test = Simultaneous False [
+        New "StaffGroup" Nothing (Simultaneous False [
+            New "Staff" Nothing (Relative c' $ Sequential [
+                Set "Staff.instrumentName" (toValue "Violin I"),
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e,
+                c, d, e
+                ]),
+            New "Staff" Nothing (Sequential [
+                Set "Staff.instrumentName" (toValue "Violin II"),
+                Clef Bass, c, g_, c])
+        ])
+    ]
+
+-- test =
+--     Simultaneous False 
+--         [ Relative g' (Sequential [
+--             addMarkup ([Bold "Hello", Italic (markup [MarkupText "cruel", Bold $ MarkupText "world"])]) rest,
+--             addArticulation Mordent $ chord [c,e,g]^*2,
+--             d^*1,
+--             e^*2,
+--             c^*(3/2),
+--             fs^*(1/2)
+--             ])
+--         , Sequential [Tremolo 4 (Sequential [c^/4,d^/4]), Tremolo 4 (Sequential [c^/4,d^/4])]
+--         , Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)]
+--         , Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)]
+--         , Relative g (Sequential [rest,c^*2,d^*1,e^*2,c^*(3/2),fs^*(1/2)])
+--         , Sequential 
+--             [ Times (4/5) (Sequential 
+--                 [
+--                     rest,
+--                     addArticulation Accent $ addPost BeginSlur $ addPost BeginCresc $ c^*2,
+--                     d^*1,
+--                     addPost Tie $ e^*1
+--                 ])
+--             , Times (4/5) (Sequential 
+--                 [      
+--                     addPost BeginDim $ addPost EndCrescDim $ e^*1,
+--                     c^*(3/2),
+--                     addPost EndSlur $ fs^*(1/2),
+--                     addPost EndCrescDim $ c^*2
+--                 ])
+--             ]
+--         ]              
         
         
     
