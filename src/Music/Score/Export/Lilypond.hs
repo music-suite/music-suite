6c3a0058bac240f7bf82e1208250736a8e91c0b8
Hans Höglund <hans@hanshoglund.se>
2013-06-09 20:58:09 +0200

    New modules for export functions
    See #52

---
+++ src/Music/Score/Export/Lilypond.hs
@@ -0,0 +1,22 @@
+                              
+{-# LANGUAGE
+    TypeFamilies,
+    DeriveFunctor,
+    DeriveFoldable,
+    GeneralizedNewtypeDeriving #-} 
+
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : non-portable (TF,GNTD)
+--
+-------------------------------------------------------------------------------------
+
+module Music.Score.Export.Lilypond -- (
+--  ) 
+where
