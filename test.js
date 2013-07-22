faaef82fb8188de990d068997937a08d1939c422
Hans Höglund <hans@hanshoglund.se>
2013-07-22 16:05:54 +0200

    Added test.js

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 
-var fs = require('fs');
+var score = null;
 
-var txt = fs.readFileSync('test.json', 'utf8');
-var val = JSON.parse(txt);                     
-console.log(val);
+function reload() {
+    var fs = require('fs');
+    var txt = fs.readFileSync('test.json', 'utf8');
+    score = JSON.parse(txt);    
+}
+
+
+/*
+
+console.log(score.staves[0].bars[0]);
+
+*/
