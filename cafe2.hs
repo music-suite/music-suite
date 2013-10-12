892500766b25c6a0c061a33c74bc3f095d31d8bb
Hans Höglund <hans@hanshoglund.se>
2013-10-12 18:10:01 +0200

    Cleaning

--- cafe2.hs
+++ cafe2.hs
@@ -42,16 +42,24 @@ import qualified Diagrams.Core.Transform as D
         - Update Monads: Cointerpreting directed containers
 -}
 
-newtype Foo m a = Foo (Writer m a)
+-- Simplified version of the standard writer monad.
+--
+newtype Write m a = Write (Writer m a)
     deriving (Monad, MonadWriter m, Functor, Foldable, Traversable)
 
-newtype Writer2 m a = Writer2 (a, m)
+{-
+Alternative implementation:
+
+newtype Write m a = Write (a, m)
     deriving (Show, Functor, Foldable, Traversable)
-instance Monoid m => Monad (Writer2 m) where
-    return x = Writer2 (x, mempty)
-    Writer2 (x1,m1) >>= f = let
-        Writer2 (x2,m2) = f x1
-        in Writer2 (x2,m1 `mappend` m2)
+instance Monoid m => Monad (Write m) where
+    return x = Write (x, mempty)
+    Write (x1,m1) >>= f = let
+        Write (x2,m2) = f x1
+        in Write (x2,m1 `mappend` m2)
+-}
+
+-- Same thing with orinary pairs:
 
 -- instance Functor ((,) a)
 deriving instance Monoid m => Foldable ((,) m)
@@ -62,23 +70,28 @@ instance Monoid m => Monad ((,) m) where
         (m2,x2) = f x1
         in (m1 `mappend` m2,x2)
 
-
-newtype RawBar m a = RawBar { getRawBar :: [(m,a)] }
-    deriving (Show, Semigroup, Monoid, Functor, Foldable, Traversable)
-instance Monoid m => Monad (RawBar m) where
-    return = RawBar . return . return
-    RawBar xs >>= f = RawBar $ xs >>= joinTrav (getRawBar . f)
-
-
-
-newtype Bar m a = Bar { getBar :: [Foo m a] }
+-- |
+-- List of values in the Write monad.
+--
+newtype WList m a = WList { getWList :: [Write m a] }
     deriving (Semigroup, Monoid, Functor, Foldable, Traversable)
-instance Monoid m => Applicative (Bar m) where
+instance Monoid m => Applicative (WList m) where
     pure = return
     (<*>) = ap
-instance Monoid m => Monad (Bar m) where
-    return = Bar . return . return
-    Bar xs >>= f = Bar $ xs >>= joinTrav (getBar . f)
+instance Monoid m => Monad (WList m) where
+    return = WList . return . return
+    WList xs >>= f = WList $ xs >>= joinTrav (getWList . f)
+
+-- Same thing with orinary pairs:
+
+newtype WList' m a = WList' { getWList' :: [(m, a)] }
+    deriving (Show, Semigroup, Monoid, Functor, Foldable, Traversable)
+instance Monoid m => Applicative (WList' m) where
+    pure = return
+    (<*>) = ap
+instance Monoid m => Monad (WList' m) where
+    return = WList' . return . return
+    WList' xs >>= f = WList' $ xs >>= joinTrav (getWList' . f)
 
 joinTrav :: (Monad t, Traversable t, Applicative f) => (a -> f (t b)) -> t a -> f (t b)
 joinTrav f = fmap join . T.traverse f
@@ -108,23 +121,23 @@ travBind f = T.traverse (bind f)
 
 -}
 
-runFoo :: Foo w a -> (a, w)
-runFoo (Foo x) = runWriter x
+runWrite :: Write w a -> (a, w)
+runWrite (Write x) = runWriter x
 
-runBar :: Bar w a -> [(a, w)]
-runBar (Bar xs) = fmap runFoo xs
+runWList :: WList w a -> [(a, w)]
+runWList (WList xs) = fmap runWrite xs
 
-tells :: Monoid m => m -> Bar m a -> Bar m a
-tells a (Bar xs) = Bar $ fmap (tell a >>) xs
+tells :: Monoid m => m -> WList m a -> WList m a
+tells a (WList xs) = WList $ fmap (tell a >>) xs
 
 
 
 
 ----------------------------------------------------------------------
 
-type Annotated a = Bar [String] a
+type Annotated a = WList [String] a
 runAnnotated :: Annotated a -> [(a, [String])]
-runAnnotated = runBar
+runAnnotated = runWList
 
 -- annotate all elements in bar
 annotate :: String -> Annotated a -> Annotated a
@@ -142,137 +155,139 @@ y = x <> annotate "a" x >>= (annotate "b" . return)
 z :: Annotated Int
 z = join $ annotate "d" $ return (annotate "c" (return 0) <> return 1)
 
--- runBar y ==> [(0,"b"),(0,"ab")]
--- runBar z ==> [(0,"dc"),(1,"d")]
+-- runWList y ==> [(0,"b"),(0,"ab")]
+-- runWList z ==> [(0,"dc"),(1,"d")]
+ 
 
 
-----------------------------------------------------------------------
-
-type Time = Double
-type Dur = Double
-
-newtype Span = Span (Time,Dur)
-    -- deriving (Semigroup,Monoid)
-
--- type TT = [String]
+-- 
+-- ----------------------------------------------------------------------
+-- 
+-- type Time = Double
+-- type Dur = Double
+-- 
+-- newtype Span = Span (Time,Dur)
+--     -- deriving (Semigroup,Monoid)
+-- 
+-- -- type TT = [String]
+-- -- 
+-- -- applyTT :: TT -> (Time, Dur) -> (Time, Dur)
+-- -- applyTT m x = (m,x)
+-- -- 
+-- -- delaying :: Time -> TT
+-- -- delaying x = return $ "delay " ++ show x
+-- -- 
+-- -- stretching :: Dur -> TT
+-- -- stretching x = return $ "stretch " ++ show x
+-- 
+-- newtype TT = TT (D.Transformation (Time,Dur))
+--     deriving (Monoid, Semigroup)
 -- 
 -- applyTT :: TT -> (Time, Dur) -> (Time, Dur)
--- applyTT m x = (m,x)
--- 
+-- applyTT t = unPoint . D.papply t . P
+--     
 -- delaying :: Time -> TT
--- delaying x = return $ "delay " ++ show x
+-- delaying x = D.translation (x,0)
 -- 
 -- stretching :: Dur -> TT
--- stretching x = return $ "stretch " ++ show x
-
-newtype TT = TT (D.Transformation (Time,Dur))
-    deriving (Monoid, Semigroup)
-
-applyTT :: TT -> (Time, Dur) -> (Time, Dur)
-applyTT t = unPoint . D.papply t . P
-    
-delaying :: Time -> TT
-delaying x = D.translation (x,0)
-
-stretching :: Dur -> TT
-stretching = D.scaling
-
--- addTT :: TT -> Bar TT a -> Bar TT a
--- addTT = tells
-
-----------------------------------------------------------------------
-
--- Compose time transformations with another Monoid
--- Generalize this pattern?
-
--- Monoid and Semigroup instances compose by default
--- 'tells' works with all Monoids
--- Need a way to generalize constructors and apply
-
-type TT2 = ([String],TT)
--- Monoid, Semigroup
-
-liftTT2 :: TT -> TT2
-liftTT2 = monR
-
-monL :: Monoid b => a -> (a, b)
-monL = swap . return
-
--- This is the Writer monad again
-monR :: Monoid b => a -> (b, a)
-monR = return
-
-type PT = () -- Semigroup, Monoid
-type DT = () -- Semigroup, Monoid
-type AT = () -- Semigroup, Monoid
-type RT = () -- Semigroup, Monoid
-
--- Nice pattern here!
-type T = ((((((),AT),RT),DT),PT),TT)
- -- Semigroup, Monoid
-idT = (mempty::T)
-
-newtype Tx = Tx T
-    deriving (Monoid, Semigroup)
-
--- TODO How to generalize applyTT2 (?)
--- All apply functions convert the monoidal transformation to
--- an endofunction (a -> a)
-
--- applyTT :: TT -> (Time, Dur) -> (Time, Dur)
--- applyPT :: PT -> Pitch     -> Pitch
--- applyDT :: PT -> Amplitude -> Amplitude
--- applyRT :: RT -> Part      -> Part
--- applyAT :: AT -> (Pitch, Amplitude) -> (Pitch, Amplitude)
--- applyST :: AT -> Point R3 -> Point R3
--- applyUnit :: () -> a -> a
-
--- This is Monoidal actions!
-
-instance Action TT Span where
-    act = applyTT
-instance (Action t a, Action u b) => Action (t, u) (a, b) where
-    act (t, u) (a, b) = (act t a, act u b)
-
-
-
-applyTT2 :: TT2 -> (Time, Dur) -> ((Time, Dur), [String])
-applyTT2 (as,t) x = (applyTT t x, as)
-    
-delaying2 :: Time -> TT2
-delaying2 x = liftTT2 $ delaying x
-
-stretching2 :: Dur -> TT2
-stretching2 x = liftTT2 $ stretching x
-
--- addTT2 :: TT2 -> Bar TT2 a -> Bar TT2 a
--- addTT2 = tells
-
-----------------------------------------------------------------------
-delay :: Time -> Bar TT a -> Bar TT a
-delay x = tells (delaying x)
-
-stretch :: Dur -> Bar TT a -> Bar TT a
-stretch x = tells (stretching x)
-
-----------------------------------------------------------------------
-
-
-type Score = Bar TT
--- Monoid, Functor, Applicative, Monad, Foldable, Traversable
-instance (IsString a, Monoid m) => IsString (Bar m a) where
-    fromString = return . fromString
-
--- runScore :: Score a -> [((Time, Time), a)]
--- runScore = fmap (swap . fmap (flip applyTT (0,1))) . runBar
-
-runScore :: Action m b => b -> Bar m a -> [(b, a)]
-runScore x = fmap (swap . fmap ((flip act) x)) . runBar
-
-foo :: Score String
-foo = stretch 2 $
-    "c" <> (delay 1 ("d" <> stretch 0.1 "e"))
-
+-- stretching = D.scaling
+-- 
+-- -- addTT :: TT -> WList TT a -> WList TT a
+-- -- addTT = tells
+-- 
+-- ----------------------------------------------------------------------
+-- 
+-- -- Compose time transformations with another Monoid
+-- -- Generalize this pattern?
+-- 
+-- -- Monoid and Semigroup instances compose by default
+-- -- 'tells' works with all Monoids
+-- -- Need a way to generalize constructors and apply
+-- 
+-- type TT2 = ([String],TT)
+-- -- Monoid, Semigroup
+-- 
+-- liftTT2 :: TT -> TT2
+-- liftTT2 = monR
+-- 
+-- monL :: Monoid b => a -> (a, b)
+-- monL = swap . return
+-- 
+-- -- This is the Writer monad again
+-- monR :: Monoid b => a -> (b, a)
+-- monR = return
+-- 
+-- type PT = () -- Semigroup, Monoid
+-- type DT = () -- Semigroup, Monoid
+-- type AT = () -- Semigroup, Monoid
+-- type RT = () -- Semigroup, Monoid
+-- 
+-- -- Nice pattern here!
+-- type T = ((((((),AT),RT),DT),PT),TT)
+--  -- Semigroup, Monoid
+-- idT = (mempty::T)
+-- 
+-- newtype Tx = Tx T
+--     deriving (Monoid, Semigroup)
+-- 
+-- -- TODO How to generalize applyTT2 (?)
+-- -- All apply functions convert the monoidal transformation to
+-- -- an endofunction (a -> a)
+-- 
+-- -- applyTT :: TT -> (Time, Dur) -> (Time, Dur)
+-- -- applyPT :: PT -> Pitch     -> Pitch
+-- -- applyDT :: PT -> Amplitude -> Amplitude
+-- -- applyRT :: RT -> Part      -> Part
+-- -- applyAT :: AT -> (Pitch, Amplitude) -> (Pitch, Amplitude)
+-- -- applyST :: AT -> Point R3 -> Point R3
+-- -- applyUnit :: () -> a -> a
+-- 
+-- -- This is Monoidal actions!
+-- 
+-- instance Action TT Span where
+--     act = applyTT
+-- instance (Action t a, Action u b) => Action (t, u) (a, b) where
+--     act (t, u) (a, b) = (act t a, act u b)
+-- 
+-- 
+-- 
+-- applyTT2 :: TT2 -> (Time, Dur) -> ((Time, Dur), [String])
+-- applyTT2 (as,t) x = (applyTT t x, as)
+--     
+-- delaying2 :: Time -> TT2
+-- delaying2 x = liftTT2 $ delaying x
+-- 
+-- stretching2 :: Dur -> TT2
+-- stretching2 x = liftTT2 $ stretching x
+-- 
+-- -- addTT2 :: TT2 -> WList TT2 a -> WList TT2 a
+-- -- addTT2 = tells
+-- 
+-- ----------------------------------------------------------------------
+-- delay :: Time -> WList TT a -> WList TT a
+-- delay x = tells (delaying x)
+-- 
+-- stretch :: Dur -> WList TT a -> WList TT a
+-- stretch x = tells (stretching x)
+-- 
+-- ----------------------------------------------------------------------
+-- 
+-- 
+-- type Score = WList TT
+-- -- Monoid, Functor, Applicative, Monad, Foldable, Traversable
+-- instance (IsString a, Monoid m) => IsString (WList m a) where
+--     fromString = return . fromString
+-- 
+-- -- runScore :: Score a -> [((Time, Time), a)]
+-- -- runScore = fmap (swap . fmap (flip applyTT (0,1))) . runWList
+-- 
+-- runScore :: Action m b => b -> WList m a -> [(b, a)]
+-- runScore x = fmap (swap . fmap ((flip act) x)) . runWList
+-- 
+-- foo :: Score String
+-- foo = stretch 2 $
+--     "c" <> (delay 1 ("d" <> stretch 0.1 "e"))
+--                                               
 
 
 -- ((0.0,2.0),"c")
