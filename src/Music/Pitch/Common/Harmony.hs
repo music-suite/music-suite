6c0c41d7cdaab8d609b0a3468a6dc79a0a1908c1
Hans Höglund <hans@hanshoglund.se>
2013-10-27 18:37:17 +0100

    Adds harmony sketches

--- src/Music/Pitch/Common/Harmony.hs
+++ src/Music/Pitch/Common/Harmony.hs
@@ -11,6 +11,11 @@ module Music.Pitch.Common.Harmony (
 import Music.Pitch.Common.Number
 import Music.Pitch.Common.Quality
 import Music.Pitch.Common.Interval
+import Music.Pitch.Literal      
+
+import Data.Semigroup
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- | Whether the given interval is a (harmonic) dissonance.
 isDissonance :: Interval -> Bool
@@ -45,3 +50,15 @@ isMelodicDissonance x = not $ isMelodicConsonance x
 -- | Whether an interval is melodic consonance.
 isMelodicConsonance :: Interval -> Bool
 isMelodicConsonance x = quality x `elem` [Perfect, Major, Minor]
+
+type Chord = Set Interval
+
+majorTriad, minorTriad :: Chord
+majorTriad = Set.fromList [_P1, _M3, _P5]
+minorTriad = Set.fromList [_P1, m3,  _P5]
+
+min7, maj7 :: Chord -> Chord
+min7 = (<> Set.fromList [m7])
+maj7 = (<> Set.fromList [_M7])
+
+
