118c71d1ba86ddfbda37997f07b5db6cec25d365
Hans Höglund <hans@hanshoglund.se>
2013-12-02 23:27:46 +0100

    Cleaning

--- src/Music/Score/Annotations.hs
+++ src/Music/Score/Annotations.hs
@@ -41,19 +41,9 @@ module Music.Score.Annotations (
 import Control.Arrow
 import Control.Monad.Plus       
 import Data.Void
-import Data.Maybe
 import Data.Semigroup
 import Data.Typeable
 import Data.String
-import Data.Set (Set)
-import Data.Map (Map)
-import Data.Foldable (Foldable)
-import Data.Traversable (Traversable)
-import qualified Data.Foldable as F
-import qualified Data.Traversable as T
-import qualified Data.List as List
-import qualified Data.Set as Set
-import qualified Data.Map as Map
 
 import Music.Time
 import Music.Time.Reactive
@@ -63,7 +53,6 @@ import Music.Score.Part
 import Music.Score.Combinators (withMetaNP)
 import Music.Score.Ornaments (HasText, text)
 import Music.Score.Meta
-import Music.Score.Util
 
 -- | 
 --   An annotation is a simple textual values attached to parts of a score.
@@ -77,20 +66,19 @@ instance IsString Annotation where fromString = Annotation . return
 annotate :: String -> Score a -> Score a
 annotate str x = annotateSpan (start >-> duration x) str x
 
--- | Annotate the whole score.
+-- | Annotate a part of the score.
 annotateSpan :: Span -> String -> Score a -> Score a
 annotateSpan span str x = addMetaNoteNP (sapp span $ return $ Annotation [str]) x
--- TODO partwise
--- XXX more sensible way of finding the part of a score
 
--- | Add the annotations to the score as text.
+-- | Show all annotations in the score.
 showAnnotations :: (HasPart' a, HasText a) => Score a -> Score a
 showAnnotations = showAnnotations' ":"
 
--- | Add the annotations to the score as text.
+-- | Show all annotations in the score using the given prefix.
 showAnnotations' :: (HasPart' a, HasText a) => String -> Score a -> Score a
 showAnnotations' prefix = withAnnotations (flip $ \s -> foldr (text . (prefix ++ )) s)
 
 -- | Handle the annotations in a score.
 withAnnotations :: (HasPart' a, HasText a) => ([String] -> Score a -> Score a) -> Score a -> Score a
 withAnnotations f = withMetaNP (f . getAnnotation)
+
