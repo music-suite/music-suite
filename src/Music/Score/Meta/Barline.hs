2f34261586a6c0f5b3f677420978e8355c2aae8d
Hans Höglund <hans@hanshoglund.se>
2013-12-07 19:41:47 +0100

    Adds fermata, rehearsal mark modules etc

---
+++ src/Music/Score/Meta/Barline.hs
@@ -0,0 +1,65 @@
+
+{-# LANGUAGE 
+    ScopedTypeVariables, 
+    GeneralizedNewtypeDeriving,
+    DeriveFunctor, 
+    DeriveFoldable, 
+    DeriveTraversable,
+    DeriveDataTypeable, 
+    ConstraintKinds,
+    FlexibleContexts, 
+    GADTs, 
+    ViewPatterns,
+    TypeFamilies,
+    MultiParamTypeClasses, 
+    FlexibleInstances #-}
+
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : non-portable (TF,GNTD)
+--
+-------------------------------------------------------------------------------------
+
+module Music.Score.Meta.Barlines (
+        -- single, double etc
+        -- repeats?
+  ) where
+
+
+import Control.Arrow
+import Control.Monad.Plus       
+import Data.Default
+import Data.Void
+import Data.Maybe
+import Data.Semigroup
+import Data.Monoid.WithSemigroup
+import Data.Typeable
+import Data.String
+import Data.Set (Set)
+import Data.Map (Map)
+import Data.Foldable (Foldable)
+import Data.Traversable (Traversable)
+import qualified Data.Foldable as F
+import qualified Data.Traversable as T
+import qualified Data.List as List
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+import Music.Time
+import Music.Time.Reactive
+import Music.Score.Note
+import Music.Score.Voice
+import Music.Score.Part
+import Music.Score.Pitch
+import Music.Score.Meta
+import Music.Score.Score
+import Music.Score.Combinators
+import Music.Score.Util
+import Music.Pitch.Literal
+
