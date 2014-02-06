e162eb523adf025b47463fbcbe0fb8cfe44854d5
Hans Höglund <hans@hanshoglund.se>
2014-02-06 17:45:25 +0100

    Use long names for import/export functions hanshoglund/music-score#149

--- src/Music/Score/Import/Sibelius.hs
+++ src/Music/Score/Import/Sibelius.hs
@@ -4,12 +4,13 @@
 
 module Music.Score.Import.Sibelius (
         IsSibelius(..),
-        fromSib,
-        readSib,
-        readSibMaybe,
-        readSibEither
+        fromSibelius,
+        readSibelius,
+        readSibeliusMaybe,
+        readSibeliusEither
   ) where
 
+import Control.Lens
 import Music.Sibelius
 import Music.Score
 import Data.Aeson
@@ -21,84 +22,92 @@ import qualified Data.ByteString.Lazy as ByteString
 -- |
 -- Convert a score from a Sibelius representation.
 --
-fromSib :: IsSibelius a => SibScore -> Score a
-fromSib (SibScore title composer info staffH transp staves systemStaff) =
-    foldr (</>) mempty $ fmap fromSibStaff staves
+fromSibelius :: IsSibelius a => SibeliusScore -> Score a
+fromSibelius (SibeliusScore title composer info staffH transp staves systemStaff) =
+    foldr (</>) mempty $ fmap fromSibeliusStaff staves
     -- TODO meta information
 
-fromSibStaff :: IsSibelius a => SibStaff -> Score a
-fromSibStaff (SibStaff bars name shortName) =
-    removeRests $ scat $ fmap fromSibBar bars
+fromSibeliusStaff :: IsSibelius a => SibeliusStaff -> Score a
+fromSibeliusStaff (SibeliusStaff bars name shortName) =
+    removeRests $ scat $ fmap fromSibeliusBar bars
     -- TODO bar length hardcoded
     -- TODO meta information
+    -- NOTE slur pos/dur always "stick" to an adjacent note, regardless of visual position
+    --      for other lines (cresc etc) this might not be the case
+    -- WARNING key sig changes goes at end of previous bar
 
-fromSibBar :: IsSibelius a => SibBar -> Score (Maybe a)
-fromSibBar (SibBar elems) = 
-    fmap Just (pcat $ fmap fromSibChordElem chords) <> return Nothing^*1
+fromSibeliusBar :: IsSibelius a => SibeliusBar -> Score (Maybe a)
+fromSibeliusBar (SibeliusBar elems) = 
+    fmap Just (pcat $ fmap fromSibeliusChordElem chords) <> return Nothing^*1
     where
         chords   = filter isChord elems
         tuplets  = filter isTuplet elems -- TODO use these
         floating = filter isFloating elems
 
-fromSibChordElem :: IsSibelius a => SibBarObject -> Score a
-fromSibChordElem = go where
-    go (SibBarObjectChord chord) = fromSibChord chord
-    go _                         = error "fromSibChordElem: Expected chord"
+fromSibeliusChordElem :: IsSibelius a => SibeliusBarObject -> Score a
+fromSibeliusChordElem = go where
+    go (SibeliusBarObjectChord chord) = fromSibeliusChord chord
+    go _                         = error "fromSibeliusChordElem: Expected chord"
 
--- handleFloatingElem :: IsSibelius a => SibBarObject -> [Score a] -> [Score a]
+-- handleFloatingElem :: IsSibelius a => SibeliusBarObject -> [Score a] -> [Score a]
 
-isChord (SibBarObjectChord _) = True
+isChord (SibeliusBarObjectChord _) = True
 isChord _                     = False
 
-isTuplet (SibBarObjectTuplet _) = True
+isTuplet (SibeliusBarObjectTuplet _) = True
 isTuplet _                      = False
 
 isFloating x = not (isChord x) && not (isTuplet x) 
     
 
-fromSibChord :: IsSibelius a => SibChord -> Score a
-fromSibChord (SibChord pos dur voice ar strem dtrem acci appo notes) = 
-    setTime $ setDur $ every setArt ar $ tremolo strem $ pcat $ fmap fromSibNote notes
-    where
+fromSibeliusChord :: IsSibelius a => SibeliusChord -> Score a
+fromSibeliusChord (SibeliusChord pos dur voice ar strem dtrem acci appo notes) = 
+    showVals $ setTime $ setDur $ every setArt ar $ tremolo strem $ pcat $ fmap fromSibeliusNote notes
+    where     
+        showVals = text (show pos ++ " " ++ show dur) -- TODO DEBUG
+        -- WARNING for tuplets, positions are absolute (sounding), but durations are relative (written)
+        -- To retrieve sounding duration we must find floating tuplet objects and use
+        -- the duration/playedDuration fields
         setTime = delay (fromIntegral pos / kTicksPerWholeNote)
         setDur  = stretch (fromIntegral dur / kTicksPerWholeNote)
         setArt Marcato         = marcato
         setArt Accent          = accent
         setArt Tenuto          = tenuto
         setArt Staccato        = staccato
-        setArt a               = error $ "fromSibChord: Unsupported articulation" ++ show a        
+        setArt a               = error $ "fromSibeliusChord: Unsupported articulation" ++ show a        
     -- TODO tremolo and appogiatura/acciaccatura support
 
-fromSibNote :: IsSibelius a => SibNote -> Score a
-fromSibNote (SibNote pitch diatonicPitch acc tied style) =
+fromSibeliusNote :: IsSibelius a => SibeliusNote -> Score a
+fromSibeliusNote (SibeliusNote pitch diatonicPitch acc tied style) =
     (if tied then fmap beginTie else id)
-    $ up' (fromIntegral pitch - 60) Pitch.c
+    $ fmap (up' (fromIntegral pitch - 60)) Pitch.c
     -- TODO spell correctly if this is Common.Pitch (how to distinguish)
     where
-        up' x = mapPitch' (+ x)
+        up' x = pitch' %~ (+ x)
+        -- up' x = mapPitch' (+ x)
 
 -- |
 -- Read a Sibelius score from a file. Fails if the file could not be read or if a parsing
 -- error occurs.
 -- 
-readSib :: IsSibelius a => FilePath -> IO (Score a)
-readSib path = fmap (either (\x -> error $ "Could not read score " ++ x) id) $ readSibEither path
+readSibelius :: IsSibelius a => FilePath -> IO (Score a)
+readSibelius path = fmap (either (\x -> error $ "Could not read score " ++ x) id) $ readSibeliusEither path
 
 -- |
 -- Read a Sibelius score from a file. Fails if the file could not be read, and returns
 -- @Nothing@ if a parsing error occurs.
 -- 
-readSibMaybe :: IsSibelius a => FilePath -> IO (Maybe (Score a))
-readSibMaybe path = fmap (either (const Nothing) Just) $ readSibEither path
+readSibeliusMaybe :: IsSibelius a => FilePath -> IO (Maybe (Score a))
+readSibeliusMaybe path = fmap (either (const Nothing) Just) $ readSibeliusEither path
 
 -- |
 -- Read a Sibelius score from a file. Fails if the file could not be read, and returns
 -- @Left m@ if a parsing error occurs.
 -- 
-readSibEither :: IsSibelius a => FilePath -> IO (Either String (Score a))
-readSibEither path = do
+readSibeliusEither :: IsSibelius a => FilePath -> IO (Either String (Score a))
+readSibeliusEither path = do
     json <- ByteString.readFile path
-    return $ fmap fromSib $ eitherDecode' json
+    return $ fmap fromSibelius $ eitherDecode' json
 
 -- |
 -- This constraint includes all note types that can be constructed from a Sibelius representation.
@@ -107,10 +116,11 @@ type IsSibelius a = (
     IsPitch a, 
     HasPart' a, 
     Enum (Part a), 
-    HasPitch a, 
+    HasPitch' a, 
     Num (Pitch a), 
     HasTremolo a, 
     HasArticulation a,
+    HasText a,
     Tiable a
     )
 
