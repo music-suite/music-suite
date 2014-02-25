fb47e48981326c3dff44ebe31b5707155fdaed4e
Hans Höglund <hans@hanshoglund.se>
2014-02-25 11:31:04 +0100

    Cleaning

--- src/Music/Score.hs
+++ src/Music/Score.hs
@@ -65,7 +65,7 @@ module Music.Score (
         module Music.Score.Import.Abc,
         module Music.Score.Import.Lilypond,
         module Music.Score.Import.Midi,
-        
+
         module Music.Score.Export.Abc,
         module Music.Score.Export.Midi,
         module Music.Score.Export.Lilypond,
@@ -73,55 +73,55 @@ module Music.Score (
 )
 where
 
-import Data.Semigroup
-import Data.Ratio
-import Control.Applicative
-import Control.Monad hiding (mapM)
-import Control.Monad.Plus
-import Data.Maybe
-import Data.Either
-import Data.Foldable
-import Data.Typeable
-import Data.Traversable
-import Data.VectorSpace hiding (Sum, getSum)
-import Data.AffineSpace
-import Data.AffineSpace.Point
-import Data.Basis
+import           Control.Applicative
+import           Control.Monad                  hiding (mapM)
+import           Control.Monad.Plus
+import           Data.AffineSpace
+import           Data.AffineSpace.Point
+import           Data.Basis
+import           Data.Either
+import           Data.Foldable
+import           Data.Maybe
+import           Data.Ratio
+import           Data.Semigroup
+import           Data.Traversable
+import           Data.Typeable
+import           Data.VectorSpace               hiding (Sum, getSum)
 
-import Music.Time
+import           Music.Time
 
-import Music.Score.Rhythm
-import Music.Score.Track
-import Music.Score.Note
-import Music.Score.Voice
-import Music.Score.Score
-import Music.Score.Combinators
-import Music.Score.Convert
-import Music.Score.Chord
-import Music.Score.Meta
-import Music.Score.Meta.Title
-import Music.Score.Meta.Attribution
-import Music.Score.Meta.RehearsalMark
-import Music.Score.Meta.Barline
-import Music.Score.Meta.Clef
-import Music.Score.Meta.Fermata
-import Music.Score.Meta.Key
-import Music.Score.Meta.Time
-import Music.Score.Meta.Tempo
-import Music.Score.Meta.Annotations
-import Music.Score.Clef
-import Music.Score.Pitch
-import Music.Score.Ties
-import Music.Score.Part
-import Music.Score.Articulation
-import Music.Score.Dynamics
-import Music.Score.Ornaments
-import Music.Score.Instances ()
-import Music.Score.Import.Abc
-import Music.Score.Import.Lilypond
-import Music.Score.Import.Midi
-import Music.Score.Export.Abc
-import Music.Score.Export.Lilypond
-import Music.Score.Export.Midi
-import Music.Score.Export.MusicXml
+import           Music.Score.Articulation
+import           Music.Score.Chord
+import           Music.Score.Clef
+import           Music.Score.Combinators
+import           Music.Score.Convert
+import           Music.Score.Dynamics
+import           Music.Score.Export.Abc
+import           Music.Score.Export.Lilypond
+import           Music.Score.Export.Midi
+import           Music.Score.Export.MusicXml
+import           Music.Score.Import.Abc
+import           Music.Score.Import.Lilypond
+import           Music.Score.Import.Midi
+import           Music.Score.Instances          ()
+import           Music.Score.Meta
+import           Music.Score.Meta.Annotations
+import           Music.Score.Meta.Attribution
+import           Music.Score.Meta.Barline
+import           Music.Score.Meta.Clef
+import           Music.Score.Meta.Fermata
+import           Music.Score.Meta.Key
+import           Music.Score.Meta.RehearsalMark
+import           Music.Score.Meta.Tempo
+import           Music.Score.Meta.Time
+import           Music.Score.Meta.Title
+import           Music.Score.Note
+import           Music.Score.Ornaments
+import           Music.Score.Part
+import           Music.Score.Pitch
+import           Music.Score.Rhythm
+import           Music.Score.Score
+import           Music.Score.Ties
+import           Music.Score.Track
+import           Music.Score.Voice
 
