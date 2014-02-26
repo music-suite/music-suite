fd10b9a11a803d660fb77b2b6f7a60d27efbe905
Hans Höglund <hans@hanshoglund.se>
2014-02-26 01:44:35 +0100

    Cleaning

--- info/update6.hs
+++ info/update6.hs
@@ -101,97 +101,16 @@ instance HasPitches a b => HasPitches [a] [b] where
     pitches = traverse . pitches
 
 
--- class HasGetPitch s where
---   getPitch :: (a ~ Pitch s) => s -> a
--- 
--- class (SetPitch (Pitch t) s ~ t) => HasSetPitch (s :: *) (t :: *) where
---   type SetPitch (b :: *) (s :: *) :: *
---   setPitch :: Pitch t -> s -> t
---   mapPitch :: (Pitch s -> Pitch t) -> s -> t
---   setPitch x = mapPitch (const x)
---   default mapPitch :: HasGetPitch s => (Pitch s -> Pitch t) -> s -> t
---   mapPitch f x = setPitch p x where p = f (getPitch x)
---   
--- type HasPitch s t = (HasGetPitch s, HasSetPitch s t)
--- 
--- type HasPitch' a = HasPitch a a
--- 
--- pitch' :: HasPitch' a => Lens' a (Pitch a)
--- pitch' = pitch
--- 
--- pitch :: HasPitch a b => Lens a b (Pitch a) (Pitch b)
--- pitch = lens getPitch (flip setPitch)
--- 
--- 
 data PitchT f a = PitchT f a
     deriving (Show, Functor, Foldable, Traversable)
 
-type instance Pitch (PitchT f a) = f
-type instance SetPitch g (PitchT f a) = PitchT g a
+type instance Pitch (PitchT p a) = p
+type instance SetPitch p' (PitchT p a) = PitchT p' a
 instance HasPitch a b => HasPitch (PitchT a c) (PitchT b c) where
-    pitch = lens (\(PitchT f a) -> f) (\(PitchT f a) g -> PitchT g a)
+    pitch = lens (\(PitchT p a) -> p) (\(PitchT p a) p' -> PitchT p' a)
 instance HasPitches a b => HasPitches (PitchT a c) (PitchT b c) where
-    pitches = lens (\(PitchT f a) -> f) (\(PitchT f a) g -> PitchT g a)
+    pitches = lens (\(PitchT p a) -> p) (\(PitchT p a) p' -> PitchT p' a)
 
--- 
--- instance (Semigroup p, Monoid p) => Applicative (PitchT p) where
---     pure = PitchT mempty
---     PitchT pf vf <*> PitchT px vx = PitchT (pf <> px) (vf $ vx)
--- 
--- type instance Pitch      (PitchT f a) = f
--- instance HasGetPitch (PitchT f a) where
---     getPitch        (PitchT f a) = f
--- 
--- instance HasSetPitch (PitchT f a) (PitchT g a)  where
---     type SetPitch g (PitchT f a) = PitchT g a 
---     setPitch      g (PitchT f a) = PitchT g a
--- 
--- -- Undecidable        
--- type instance Pitch [a] = Pitch a
--- instance HasSetPitch a b => HasSetPitch [a] [b] where
---     type SetPitch b [a] = [SetPitch b a]
---     mapPitch f = fmap (mapPitch f)
--- 
--- type instance Pitch (c,a) = Pitch a
--- instance HasGetPitch a => HasGetPitch (c,a) where
---     getPitch (c,a) = getPitch a
--- 
--- -- Undecidable ??
--- instance HasSetPitch a b => HasSetPitch (c,a) (c,b) where
---     type SetPitch b (c,a) = (c,SetPitch b a)
---     mapPitch f = fmap (mapPitch f)
--- 
--- 
--- 
--- -- instance HasGetPitch M.Pitch where
--- --     type Pitch M.Pitch = M.Pitch
--- --     getPitch = id
--- -- instance HasSetPitch M.Pitch a where
--- --     type SetPitch a M.Pitch = a
--- --     setPitch = const
--- 
--- type instance Pitch Int = Int
--- instance HasGetPitch Int where
---     getPitch = id
--- instance (a ~ Pitch a) => HasSetPitch Int a where
---     type SetPitch a Int = a
---     mapPitch = id
--- 
--- type instance Pitch Bool = Bool
--- instance HasGetPitch Bool where
---     getPitch = id
--- instance (a ~ Pitch a) => HasSetPitch Bool a where
---     type SetPitch a Bool = a
---     mapPitch = id
--- 
--- type instance Pitch M.Pitch = M.Pitch
--- instance HasGetPitch M.Pitch where
---     getPitch = id
--- instance (a ~ Pitch a) => HasSetPitch M.Pitch a where
---     type SetPitch a M.Pitch = a
---     mapPitch = id
--- 
--- 
 
 type Interval a = Diff (Pitch a)
 
