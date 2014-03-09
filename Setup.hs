030ac706f070ae0426eb427274df29e2ee64c6b4
Hans Höglund <hans@hanshoglund.se>
2014-03-09 14:56:09 +0100

    Actually check in Util file #164
    Closes #164

--- Setup.hs
+++ Setup.hs
@@ -1,11 +1,14 @@
 #! /usr/bin/env runhaskell
 
-import Data.Monoid
-import System.Process
 import Distribution.Simple
+main = defaultMain
 
-main = defaultMainWithHooks simpleUserHooks  { preBuild = preBuild' }
-
-preBuild' _ _ = do
-    runCommand "hackette cp Music.Score.Util src/Music/Score/Util.hs"
-    return mempty
+-- import Data.Monoid
+-- import System.Process
+-- import Distribution.Simple
+-- 
+-- main = defaultMainWithHooks simpleUserHooks  { preBuild = preBuild' }
+-- 
+-- preBuild' _ _ = do
+--     runCommand "hackette cp Music.Score.Util src/Music/Score/Util.hs"
+--     return mempty
