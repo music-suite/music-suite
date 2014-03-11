a097357459e1aa9a28bbc014e6e556a0781d9a25
Hans Höglund <hans@hanshoglund.se>
2014-03-11 22:05:24 +0100

    Sketches

--- info/merge.hs
+++ info/merge.hs
@@ -1,26 +1,26 @@
 {-# LANGUAGE 
     DeriveFunctor #-}
 
-import Data.List (sort)
+import Data.Stream (sort)
 
 import Data.Semigroup
 
 -- This works
 --      foldr (<>) mempty $ fmap single $ [1..]
 
-newtype List a = List { getList :: [a] }
+newtype Stream a = Stream { getStream :: [a] }
     deriving (Eq, Ord, Show, Functor)
-instance Ord a => Semigroup (List a) where
-    List ~a <> List ~b = List (a `m` b)
+instance Ord a => Semigroup (Stream a) where
+    Stream ~a <> Stream ~b = Stream (a `m` b)
         where
             m = merge
             -- m = (<>)
-unlist (List ~a) = a
+unStream (Stream ~a) = a
 inf = [1..]
-infL = List [1..]
+infL = Stream [1..]
 
-instance Ord a => Monoid (List a) where
-    mempty = List mempty
+instance Ord a => Monoid (Stream a) where
+    mempty = Stream mempty
     mappend = (<>)
 
 merge2 a b = sort $ a ++ b
@@ -32,4 +32,4 @@ merge xs'@(x:xs) ys'@(y:ys) | x < y   =  x : merge xs ys'
                             | x >= y  =  y : merge xs' ys
                             
 single x = [x]
-singleL x = List [x]
+singleL x = Stream [x]
