7846da6d32e5b4d911f540982a948b3cdabe9d91
Hans Höglund <hans@hanshoglund.se>
2014-03-13 01:35:16 +0100

    Test drawing scores and behaviors

--- info/design.hs
+++ info/design.hs
@@ -10,6 +10,7 @@ import Music.Prelude.Basic hiding (pitch)
 import qualified Music.Pitch as P
 import qualified Music.Score as S
 
+import Data.Default
 import Data.VectorSpace
 import Data.AffineSpace
 -- import Data.Semigroup
@@ -31,8 +32,16 @@ durationToDouble = realToFrac
 pitchToDouble :: P.Pitch -> Double
 pitchToDouble = realToFrac . semitones . (.-. c)
 
-drawScore :: (Renderable (Path R2) b, Real a) => Score a -> Diagram b R2
-drawScore = {-bg whitesmoke .-} scaleX 1{-TODO-} . mconcat . 
+-- showOr = (<> (fc red $ circle 2))
+showOr = id
+
+drawBeh :: (Renderable (Path R2) b, Real a) => Behavior a -> Diagram b R2
+drawBeh b = cubicSpline False points
+    where
+        points = take 200 $ fmap (\x -> p2 (x, realToFrac $ b ? realToFrac x)) [0,0.1..]
+
+drawPart :: (Renderable (Path R2) b, Real a) => Score a -> Diagram b R2
+drawPart = (<> alignL (hrule 20)) . showOr . alignTL . scaleX 1{-TODO-} . mconcat . 
     fmap drawScoreNote . 
     fmap (map1 timeToDouble . map2 durationToDouble . map3 realToFrac) . 
     (^. events)
@@ -43,16 +52,15 @@ drawScore = {-bg whitesmoke .-} scaleX 1{-TODO-} . mconcat .
         drawScoreNote (t,d,x) = translateY x $ translateX (t.+^(d^/2)) $ scaleX d $ noteShape
         noteShape = lcA transparent $ fcA (blue `withOpacity` 0.5) $ square 1
 
--- TODO
-drawBeh :: (Renderable (Path R2) b, Real a) => Behavior a -> Diagram b R2
-drawBeh = const $ circle 1
 
 
+grid = alignTL $ hcat' (def & sep +~ 1) $ replicate 20 $ vrule 50
+main = openGraphic $ (<> drawBeh (fmap (sin.(*(tau/10)).realToFrac) $ varying id)) $ (<> grid) $ vcat' (def & sep +~ 8) . fmap (drawPart . fmap toSemi) . extractParts $ score1
+    where
+        toSemi = (semitones.(.-. c).__getPitch)
 
-main = openGraphic $ drawScore $ fmap (semitones.(.-. c).__getPitch) $ score1
-
-score1 :: Score Note
-score1 = rcat [
+score1 :: Score BasicNote
+score1 = compress 4 $ rcat [
     up _P5 $ stretch 2 $ scat $ fmap motive $ [1..5],
     up _M3 $ stretch 3 $ scat $ fmap motive $ [1..4],
     up _P1 $ stretch 4 $ scat $ fmap motive $ [1..3]
