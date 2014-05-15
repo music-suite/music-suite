789edcdd01648fc11a1a12952696819ee1e17de2
Hans Höglund <hans@hanshoglund.se>
2014-05-15 15:37:40 +0200

    Cleaning

--- src/Music/Score/Ties.hs
+++ src/Music/Score/Ties.hs
@@ -64,32 +64,32 @@ import           Music.Dynamics.Literal
 -- Minimal definition: 'toTied', or both 'beginTie' and 'endTie'.
 --
 class Tiable a where
-    -- |
-    -- Modify a note to be the first note in a tied note pair.
-    --
-    beginTie :: a -> a
-    beginTie = fst . toTied
+  -- |
+  -- Modify a note to be the first note in a tied note pair.
+  --
+  beginTie :: a -> a
+  beginTie = fst . toTied
 
-    -- |
-    -- Modify a note to be the second note in a tied note pair.
-    --
-    endTie :: a -> a
-    endTie = snd . toTied
+  -- |
+  -- Modify a note to be the second note in a tied note pair.
+  --
+  endTie :: a -> a
+  endTie = snd . toTied
 
-    -- |
-    -- Split a single note into a pair of tied notes.
-    --
-    -- The first returned element should have the original 'onset' and the second
-    -- element should have the original 'offset'. Formally
-    --
-    -- > (onset . fst . toTied) a = onset a
-    -- > (offset . snd . toTied) a = offset a
-    --
-    toTied    :: a -> (a, a)
-    toTied a = (beginTie a, endTie a)
+  -- |
+  -- Split a single note into a pair of tied notes.
+  --
+  -- The first returned element should have the original 'onset' and the second
+  -- element should have the original 'offset'. Formally
+  --
+  -- > (onset . fst . toTied) a = onset a
+  -- > (offset . snd . toTied) a = offset a
+  --
+  toTied    :: a -> (a, a)
+  toTied a = (beginTie a, endTie a)
 
 newtype TieT a = TieT { getTieT :: ((Any, Any), a) }
-    deriving (Eq, Ord, Show, Functor, Foldable, Typeable, Applicative, Monad)
+  deriving (Eq, Ord, Show, Functor, Foldable, Typeable, Applicative, Monad)
 
 -- | Unsafe: Do not use 'Wrapped' instances
 instance Wrapped (TieT a) where
@@ -106,88 +106,88 @@ instance Tiable ()          where { beginTie = id ; endTie = id }
 instance Tiable (Ratio a)   where { beginTie = id ; endTie = id }
 
 instance Tiable a => Tiable (TieT a) where
-    toTied (TieT ((prevTie, nextTie), a))   = (TieT ((prevTie, Any True), b), TieT ((Any True, nextTie), c))
-         where (b,c) = toTied a
+  toTied (TieT ((prevTie, nextTie), a))   = (TieT ((prevTie, Any True), b), TieT ((Any True, nextTie), c))
+       where (b,c) = toTied a
 
 instance Tiable a => Tiable [a] where
-    toTied = unzip . fmap toTied
+  toTied = unzip . fmap toTied
 
 instance Tiable a => Tiable (Behavior a) where
-    toTied = unzipR . fmap toTied
+  toTied = unzipR . fmap toTied
 
 -- 
 -- There is no (HasPart ChordT) instance, so PartT must be outside ChordT in the stack
 -- This restriction assures all chord notes are in the same part
 -- 
 instance Tiable a => Tiable (c, a) where
-    toTied = unzipR . fmap toTied
+  toTied = unzipR . fmap toTied
 
 instance Tiable a => Tiable (Maybe a) where
-    toTied = unzipR . fmap toTied
+  toTied = unzipR . fmap toTied
 
 instance Tiable a => Tiable (Sum a) where
-    toTied = unzipR . fmap toTied
+  toTied = unzipR . fmap toTied
 
 instance Tiable a => Tiable (Product a) where
-    toTied = unzipR . fmap toTied
+  toTied = unzipR . fmap toTied
 
 -- Lifted instances
 
 instance IsPitch a => IsPitch (TieT a) where
-    fromPitch = pure . fromPitch
+  fromPitch = pure . fromPitch
 
 instance IsDynamics a => IsDynamics (TieT a) where
-    fromDynamics = return . fromDynamics
+  fromDynamics = return . fromDynamics
 
 instance Transformable a => Transformable (TieT a) where
-    transform s = fmap (transform s)
+  transform s = fmap (transform s)
 
 instance Reversible a => Reversible (TieT a) where
-    rev = fmap rev
+  rev = fmap rev
 
 instance Num a => Num (TieT a) where
-    (+) = liftA2 (+)
-    (*) = liftA2 (*)
-    (-) = liftA2 (-)
-    abs = fmap abs
-    signum = fmap signum
-    fromInteger = pure . fromInteger
+  (+) = liftA2 (+)
+  (*) = liftA2 (*)
+  (-) = liftA2 (-)
+  abs = fmap abs
+  signum = fmap signum
+  fromInteger = pure . fromInteger
 
 instance Fractional a => Fractional (TieT a) where
-    recip        = fmap recip
-    fromRational = pure . fromRational
+  recip        = fmap recip
+  fromRational = pure . fromRational
 
 instance Floating a => Floating (TieT a) where
-    pi    = pure pi
-    sqrt  = fmap sqrt
-    exp   = fmap exp
-    log   = fmap log
-    sin   = fmap sin
-    cos   = fmap cos
-    asin  = fmap asin
-    atan  = fmap atan
-    acos  = fmap acos
-    sinh  = fmap sinh
-    cosh  = fmap cosh
-    asinh = fmap asinh
-    atanh = fmap atanh
-    acosh = fmap acos
+  pi    = pure pi
+  sqrt  = fmap sqrt
+  exp   = fmap exp
+  log   = fmap log
+  sin   = fmap sin
+  cos   = fmap cos
+  asin  = fmap asin
+  atan  = fmap atan
+  acos  = fmap acos
+  sinh  = fmap sinh
+  cosh  = fmap cosh
+  asinh = fmap asinh
+  atanh = fmap atanh
+  acosh = fmap acos
 
 instance Enum a => Enum (TieT a) where
-    toEnum = pure . toEnum
-    fromEnum = fromEnum . get1
+  toEnum = pure . toEnum
+  fromEnum = fromEnum . get1
 
 instance Bounded a => Bounded (TieT a) where
-    minBound = pure minBound
-    maxBound = pure maxBound
+  minBound = pure minBound
+  maxBound = pure maxBound
 
 instance (Num a, Ord a, Real a) => Real (TieT a) where
-    toRational = toRational . get1
+  toRational = toRational . get1
 
 instance (Real a, Enum a, Integral a) => Integral (TieT a) where
-    quot = liftA2 quot
-    rem = liftA2 rem
-    toInteger = toInteger . get1  
+  quot = liftA2 quot
+  rem = liftA2 rem
+  toInteger = toInteger . get1  
 
 -- |
 -- Split all notes that cross a barlines into a pair of tied notes.
@@ -196,12 +196,12 @@ splitTiesVoice :: Tiable a => Voice a -> Voice a
 splitTiesVoice = (^. voice) . map (^. stretched) 
   . concat . snd . List.mapAccumL g 0 
   . map (^. from stretched) . (^. stretcheds)
-    where
-        g t (d, x) = (t + d, occs)
-            where
-                (_, barTime) = properFraction t
-                remBarTime   = 1 - barTime
-                occs         = splitDurThen remBarTime 1 (d,x)
+  where
+    g t (d, x) = (t + d, occs)
+      where
+        (_, barTime) = properFraction t
+        remBarTime   = 1 - barTime
+        occs         = splitDurThen remBarTime 1 (d,x)
 
 -- |
 -- Split all voice into bars, using the given bar durations. Music that does not
@@ -216,8 +216,8 @@ splitTiesVoiceAt' :: Tiable a => [Duration] -> [(Duration, a)] -> [[(Duration, a
 splitTiesVoiceAt' []  _  =  []
 splitTiesVoiceAt' _  []  =  []
 splitTiesVoiceAt' (barDur : rbarDur) occs = case splitDurFor barDur occs of
-    (barOccs, [])       -> barOccs : []
-    (barOccs, restOccs) -> barOccs : splitTiesVoiceAt' rbarDur restOccs
+  (barOccs, [])       -> barOccs : []
+  (barOccs, restOccs) -> barOccs : splitTiesVoiceAt' rbarDur restOccs
 
 tsplitTiesVoiceAt :: [Duration] -> [Duration] -> [[(Duration, Char)]]
 tsplitTiesVoiceAt barDurs = fmap (map (^. from stretched) . (^. stretcheds)) . splitTiesVoiceAt barDurs . ((^. voice) . map (^. stretched)) . fmap (\x -> (x,'_'))
@@ -231,8 +231,8 @@ tsplitTiesVoiceAt barDurs = fmap (map (^. from stretched) . (^. stretcheds)) . s
 --
 splitDurThen :: Tiable a => Duration -> Duration -> (Duration, a) -> [(Duration, a)]
 splitDurThen s t x = case splitDur s x of
-    (a, Nothing) -> [a]
-    (a, Just b)  -> a : splitDurThen t t b
+  (a, Nothing) -> [a]
+  (a, Just b)  -> a : splitDurThen t t b
 
 
 -- |
@@ -250,17 +250,17 @@ splitDurThen s t x = case splitDur s x of
 splitDurFor :: Tiable a => Duration -> [(Duration, a)] -> ([(Duration, a)], [(Duration, a)])
 splitDurFor remDur []       = ([], [])
 splitDurFor remDur (x : xs) = case splitDur remDur x of
-    (x@(d,_), Nothing)   ->
-        if d < remDur then
-            first (x:) $ splitDurFor (remDur - d) xs
-        else -- d == remDur
-            ([x], xs)
-    (x@(d,_), Just rest) -> ([x], rest : xs)
+  (x@(d,_), Nothing)   ->
+      if d < remDur then
+          first (x:) $ splitDurFor (remDur - d) xs
+      else -- d == remDur
+          ([x], xs)
+  (x@(d,_), Just rest) -> ([x], rest : xs)
 
 tsplitDurFor :: Duration -> [Duration] -> ([(Duration,Char)], [(Duration,Char)])
 tsplitDurFor maxDur xs = splitDurFor maxDur $ fmap (\x -> (x,'_')) xs
 instance Tiable Char where
-    toTied _ = ('(',')')
+  toTied _ = ('(',')')
 
 -- |
 -- Split a note if it is longer than the given duration. Returns the first part of the
@@ -270,9 +270,9 @@ instance Tiable Char where
 --
 splitDur :: Tiable a => Duration -> (Duration, a) -> ((Duration, a), Maybe (Duration, a))
 splitDur maxDur (d,a)
-    | maxDur <= 0 = error "splitDur: maxDur must be > 0"
-    | d <= maxDur =  ((d, a), Nothing)
-    | d >  maxDur =  ((maxDur, b), Just (d - maxDur, c)) where (b,c) = toTied a
+  | maxDur <= 0 = error "splitDur: maxDur must be > 0"
+  | d <= maxDur =  ((d, a), Nothing)
+  | d >  maxDur =  ((maxDur, b), Just (d - maxDur, c)) where (b,c) = toTied a
 
 
 -- TODO use extract
