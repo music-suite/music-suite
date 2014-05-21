883dc7a38bf09dd801a988e928374090d592423a
Hans Höglund <hans@hanshoglund.se>
2014-05-22 00:51:47 +0200

    Adds documentation for Music.Time.Rest

--- src/Music/Time/Rest.hs
+++ src/Music/Time/Rest.hs
@@ -20,6 +20,15 @@
 -- Stability   : experimental
 -- Portability : non-portable (TF,GNTD)
 --
+-- Many time structures such as 'Score' allows for rests between notes. Generally rests
+-- are simply treated as blank space, and thus have no duration. Sometimes it is useful
+-- to represent rests explicitly, so this module provides an alias for 'pure' 'Nothing' that
+-- can be used to that end.
+-- 
+-- To remove rests from a score, use 'mcatMaybes', for example:
+--
+-- > open $ mcatMaybes $ scat [c,d,rest^*2,e]^/8
+-- 
 -------------------------------------------------------------------------------------
 
 module Music.Time.Rest (
@@ -27,7 +36,6 @@ module Music.Time.Rest (
   ) where
 
 import           Control.Applicative
-
 import           Music.Time.Reverse
 import           Music.Time.Split
 
