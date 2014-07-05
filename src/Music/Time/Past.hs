1262eb4f3ae99f1933bef46b9d58a1c116edb007
Hans Höglund <hans@hanshoglund.se>
2014-07-05 03:09:43 +0200

    Sketch Time.Past

--- src/Music/Time/Past.hs
+++ src/Music/Time/Past.hs
@@ -1,2 +1,15 @@
 
 module Music.Time.Past where
+
+-- |
+-- Past represents a value occuring /up to/ some point in time.
+--
+-- It may be seen as a note whose era is a left-open time interval.
+--
+newtype Past a = Past { getPast :: (a, Time) }
+
+past :: Past a -> Time -> Maybe a
+past (Past (t, x)) t'
+  | if t' <= t = Just x
+  | otherwise  = Nothing
+
