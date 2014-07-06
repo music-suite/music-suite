5db9e11b6a8a81e5b854f013cdcc2d6e083dd2e8
Hans Höglund <hans@hanshoglund.se>
2014-07-07 00:36:11 +0200

    Clean Setup.lhs

--- Setup.hs
+++ Setup.hs
@@ -2,13 +2,3 @@
 
 import Distribution.Simple
 main = defaultMain
-
--- import Data.Monoid
--- import System.Process
--- import Distribution.Simple
--- 
--- main = defaultMainWithHooks simpleUserHooks  { preBuild = preBuild' }
--- 
--- preBuild' _ _ = do
---     runCommand "hackette cp Music.Score.Util src/Music/Score/Util.hs"
---     return mempty
