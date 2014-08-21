83c700855fddb57951819ab893dab5450d8d64f4
Hans Höglund <hans@hanshoglund.se>
2014-08-21 18:41:56 +0200

    Fix module descriptions

--- src/Music/Lilypond/IO.hs
+++ src/Music/Lilypond/IO.hs
@@ -1,6 +1,18 @@
 
 {-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
 
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : GHC
+--
+-------------------------------------------------------------------------------------
+
 module Music.Lilypond.IO -- (
   -- )
 where
