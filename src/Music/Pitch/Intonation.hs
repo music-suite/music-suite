d084ac1cb3372b4e056eed6eec9bc1d9fd3bdd91
Hans Höglund <hans@hanshoglund.se>
2014-09-29 00:25:07 +0100

    Rename IntoneInterval -> Tuning

--- src/Music/Pitch/Intonation.hs
+++ src/Music/Pitch/Intonation.hs
@@ -15,7 +15,7 @@
 
 module Music.Pitch.Intonation (
       Intonation,
-      IntoneInterval,
+      Tuning,
 
       tuneAbsolute,
       -- makeBasis,
@@ -35,8 +35,8 @@ module Music.Pitch.Intonation (
       fiftyThreeToneEqual,
 
       -- * Specific intonations
-      standardTuning,
-      -- standardIntonation,
+      -- standardTuning,
+      standardIntonation,
 )
 where
 
@@ -54,7 +54,7 @@ import Music.Pitch.Common.Interval
 import Music.Pitch.Common.Pitch
 
 type Intonation p = p -> Hertz
-type IntoneInterval i = i -> FreqRatio
+type Tuning i = i -> FreqRatio
 
 synTune :: (Interval, FreqRatio) -> (Interval, FreqRatio) -> Interval -> FreqRatio
 synTune (i1, i1rat) (i2, i2rat) (Interval (a1, d2)) =
@@ -68,29 +68,43 @@ makeBasis i (i1, r1) (i2, r2) = case (convertBasisFloat i i1 i2) of
   Nothing -> error ("Cannot use intervals " ++ (show i1) ++ " and " ++ (show i2) ++ " as basis pair to represent " ++ (show i))
 
 
-tuneAbsolute :: IntoneInterval Interval -> (Pitch, Hertz) -> Pitch -> Hertz
+tuneAbsolute :: Tuning Interval -> (Pitch, Hertz) -> Pitch -> Hertz
 tuneAbsolute t (b, f) p = f .+^ (t i) where i = p .-. b
 
 -- Standard syntonic (meantone) tunings, with P8 = 2
 
 pureOctaveWith = synTune (_P8, 2)
 
+pythagorean :: Tuning Interval
 pythagorean = pureOctaveWith (_P5, 3/2)
 
+quarterCommaMeantone :: Tuning Interval
 quarterCommaMeantone = pureOctaveWith (_M3, 5/4)
 
+schismaticMeantone :: Tuning Interval
 schismaticMeantone = pureOctaveWith (8 *^ _P4, 10)
 
 -- TET tunings, i.e. where P8 = 2 and (some other interval) = 1
 
 tetTune i = pureOctaveWith (i, 1)
 
+fiveToneEqual :: Tuning Interval
 fiveToneEqual = tetTune m2
+
+sevenToneEqual :: Tuning Interval
 sevenToneEqual = tetTune _A1
+
+twelveToneEqual :: Tuning Interval
 twelveToneEqual = tetTune d2
+
+nineteenToneEqual :: Tuning Interval
 nineteenToneEqual = tetTune dd2 where dd2 = d2 ^-^ _A1
+
+thirtyOneToneEqual :: Tuning Interval
 thirtyOneToneEqual = tetTune dddd3 where dddd3 = m3 ^-^ (4 *^ _A1)
+
+fiftyThreeToneEqual :: Tuning Interval
 fiftyThreeToneEqual = tetTune ddddddd6 where ddddddd6 = 31 *^ _P8 ^-^ 53 *^ _P5 -- (!)
 
-standardTuning :: Intonation Pitch
-standardTuning = tuneAbsolute twelveToneEqual (a, 440)
+standardIntonation :: Intonation Pitch
+standardIntonation = tuneAbsolute twelveToneEqual (a, 440)
