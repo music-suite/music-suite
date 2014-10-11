ae2eb44422dc5f8a5698351689c9a6a64fba556b
Hans Höglund <hans@hanshoglund.se>
2014-10-11 15:12:13 +0100

    Cleaning

--- src/Music/Time/Future.hs
+++ src/Music/Time/Future.hs
@@ -1,6 +1,19 @@
 
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012-2014
+--
+-- License     : BSD-style
+--
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : non-portable (TF,GNTD)
+--
+-------------------------------------------------------------------------------------
+
 module Music.Time.Future (
     module Music.Time.Past
 ) where
 
+-- Future and Past are defined together in this module for now. 
 import Music.Time.Past
