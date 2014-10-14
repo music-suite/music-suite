e72d609673e253741ebacf2e80760463b5cbb2d7
Hans Höglund <hans@hanshoglund.se>
2014-10-14 19:38:09 +0100

    Cleaning

--- src/Music/Pitch/Equal.hs
+++ src/Music/Pitch/Equal.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 -------------------------------------------------------------------------------------
 -- |
@@ -20,27 +19,26 @@
 -------------------------------------------------------------------------------------
 
 module Music.Pitch.Equal (
+    -- * Equal temperament
     Equal,
+    toEqual,
+    fromEqual,
+    size,
+    cast,
+
+    -- ** Synonyms
     Equal6,
     Equal12,
     Equal17,
     Equal24,
     Equal36,
-    toEqual,
-    unsafeToEqual,
-    fromEqual,
-    size,
-    -- cast,
+
+    -- ** Extra type-level naturals
+    N17,
+    N24,
+    N36,
 )
 where
-
--- {-
--- 
--- TODO *avoid* mixing things up here
--- 
---   This type implements limited values (useful for interval *steps*)
---   An ET-interval is just an int, with a type-level size (divMod is "separate")
--- -}
 
 import Data.Maybe
 import Data.Either
@@ -53,14 +51,6 @@ import TypeUnary.Nat
 
 -- Based on Data.Fixed
 
-  
--- 
--- import Data.TypeLevel.Num hiding ((<), (>), (+), (<=), (*))
--- import qualified Data.TypeLevel.Num as TL
--- 
--- data Foo = Foo Int deriving Show
--- 
-
 newtype Equal a = Equal { fromEqual :: Int }
 
 deriving instance {-IsNat a =>-} Eq (Equal a)
@@ -72,17 +62,14 @@ instance {-IsNat a =>-} Show (Equal a) where
     where app_prec = 10
 
 instance IsNat a => Num (Equal a) where
-  fromInteger = unsafeToEqual . fromIntegral
+  fromInteger = toEqual . fromIntegral
 
--- | Convenience to avoid ScopedTypeVariables etc    
+-- Convenience to avoid ScopedTypeVariables etc    
 getSize :: IsNat a => Equal a -> Nat a
 getSize _ = nat 
 
-checkSize :: IsNat a => Equal a -> Maybe (Equal a)
-checkSize x = if 0 <= fromEqual x && fromEqual x < size x then Just x else Nothing
-
--- 
 -- | Size of this type (value not evaluated).
+-- 
 -- >>> size (undefined :: Equal N2)
 -- 2
 -- 
@@ -92,13 +79,39 @@ checkSize x = if 0 <= fromEqual x && fromEqual x < size x then Just x else Nothi
 size :: IsNat a => Equal a -> Int
 size = natToZ . getSize
 
-toEqual :: IsNat a => Int -> Maybe (Equal a)
-toEqual = checkSize . Equal
+-- TODO I got this part wrong
+-- 
+-- This type implements limited values (useful for interval *steps*)
+-- An ET-interval is just an int, with a type-level size (divMod is "separate")
 
-unsafeToEqual :: IsNat a => Int -> Equal a
-unsafeToEqual n = case toEqual n of
-  Nothing -> error $ "Bad equal: " ++ show n
-  Just x  -> x
+-- -- | Create an equal-temperament value.
+-- toEqual :: IsNat a => Int -> Maybe (Equal a)
+-- toEqual = checkSize . Equal
+-- 
+-- -- | Unsafely create an equal-temperament value.
+-- unsafeToEqual :: IsNat a => Int -> Equal a
+-- unsafeToEqual n = case toEqual n of
+--   Nothing -> error $ "Bad equal: " ++ show n
+--   Just x  -> x
+-- 
+-- checkSize :: IsNat a => Equal a -> Maybe (Equal a)
+-- checkSize x = if 0 <= fromEqual x && fromEqual x < size x then Just x else Nothing
+-- 
+
+-- | Create an equal-temperament value.
+toEqual :: IsNat a => Int -> Equal a
+toEqual = Equal
+
+-- | Safely cast a tempered value to a larger size.
+--
+-- >>> cast (1 :: Equal12) :: Equal24
+-- Equal 2 :: Equal24
+--
+cast :: (IsNat a, IsNat b) => Equal a -> Equal b
+cast = cast' undefined
+
+cast' :: (IsNat a, IsNat b) => Equal b -> Equal a -> Equal b
+cast' bDummy aDummy@(Equal a) = Equal $ (a * size bDummy) `div` size aDummy
 
 type Equal6  = Equal N6
 type Equal12 = Equal N12
@@ -112,13 +125,3 @@ type N17 = N10 :+: N7
 type N24 = N20 :+: N4
 type N36 = N30 :+: N6
 
--- | Safely cast a tempered value to a larger size.
---
--- >>> cast (1 :: Equal12) :: Equal24
--- Equal 2 :: Equal24
---
-cast :: (IsNat a, IsNat b) => Equal a -> Equal b
-cast = cast' undefined
-
-cast' :: (IsNat a, IsNat b) => Equal b -> Equal a -> Equal b
-cast' bDummy aDummy@(Equal a) = Equal $ (a * size bDummy) `div` size aDummy
