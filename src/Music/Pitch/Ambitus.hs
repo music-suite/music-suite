72593dbf80f94801795764f9de1943fca45c5b05
Hans Höglund <hans@hanshoglund.se>
2014-10-16 17:17:24 +0100

    Add basic Ambitus
    Closes #34

--- src/Music/Pitch/Ambitus.hs
+++ src/Music/Pitch/Ambitus.hs
@@ -1,8 +1,15 @@
 
 module Music.Pitch.Ambitus where
 
-type Interv a = (a,a) -- TODO choose library
+import Data.Interval hiding (Interval, interval)
+import qualified Data.Interval as Interval
 
 -- | An ambitus is a closed interval (in the mathematical sense).
-type Ambitus a = Interv a
+--
+--   Also known as /range/ or /tessitura/, this type can be used to restrict the
+--   range of a melody, chord or other pitch container.
+--
+--   It is also used in "Music.Parts" to represent the range of instruments.
+--
+type Ambitus a = Interval.Interval a
 
