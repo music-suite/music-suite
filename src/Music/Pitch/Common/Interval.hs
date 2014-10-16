c826ee4d39537dba83e65538a1f69a72d5b379bc
Hans Höglund <hans@hanshoglund.se>
2014-10-16 19:48:33 +0100

    Add sketch of new Interval stuff
    Related to fixes of #41, #40, #29, #25 and #24
    
    TODO integrate with previous code, test and cleanup

--- src/Music/Pitch/Common/Interval.hs
+++ src/Music/Pitch/Common/Interval.hs
@@ -632,9 +632,8 @@ octaves i
   | isNegative i && not (isOctaveMultiple i) = negate (octaves' i) - 1
   | isNegative i && isOctaveMultiple i       = negate (octaves' i)
   | otherwise                                = octaves' i
-
+-- where:
 isOctaveMultiple (Interval (_,d)) = d `mod` 7 == 0
-
 octaves' i = fromIntegral $ intervalDiv i basis_P8
 
 -- |
@@ -726,6 +725,190 @@ invert = simple . negate
 --
 asInterval :: Interval -> Interval
 asInterval = id
+
+
+
+
+
+
+
+
+{-
+data QualityType = PerfectType | MajorMinorType
+  deriving (Eq, Ord, Read, Show)
+
+expectedQualityType :: HasNumber a => a -> QualityType
+expectedQualityType x = if ((abs (number x) - 1) `mod` 7) + 1 `elem` [1,4,5]
+  then PerfectType else MajorMinorType
+
+qualityTypes :: Quality -> [QualityType]
+qualityTypes Perfect = [PerfectType]
+qualityTypes Major   = [MajorMinorType]
+qualityTypes Minor   = [MajorMinorType]
+qualityTypes _       = [PerfectType, MajorMinorType]
+
+-- FIXME problem that this treats major as neutral, while this only holds for positive intervals
+qualityToDiff :: Bool -> QualityType -> Quality -> ChromaticSteps
+qualityToDiff positive qt q = fromIntegral $ go positive qt q
+  where
+    go True MajorMinorType (Augmented n)  = 0 + n
+    go True MajorMinorType Major          = 0
+    go True MajorMinorType Minor          = (-1)
+    go True MajorMinorType (Diminished n) = (-1) - n
+
+    go False MajorMinorType (Augmented n)  = 1 + n
+    go False MajorMinorType Major          = 1
+    go False MajorMinorType Minor          = 0
+    go False MajorMinorType (Diminished n) = 0 - n
+    
+    go _ PerfectType (Augmented n)  = 0 + n
+    go _ PerfectType Perfect        = 0
+    go _ PerfectType (Diminished n) = 0 - n
+    
+    go _ qt q = error $ "qualityToDiff: Unknown interval expression (" ++ show qt ++ ", " ++ show q ++ ")"
+
+mkInterval2 :: Quality -> Number -> Interval
+mkInterval2 q n = mkInterval' (fromIntegral diff) (fromIntegral steps)
+  where
+    diff  = qualityToDiff (n > 0) (expectedQualityType n) (q)
+    steps = case n `compare` 0 of
+      GT -> n - 1
+      EQ -> error "diatonicSteps: Invalid number 0"
+      LT -> n + 1
+    -- steps = n^.diatonicSteps
+
+upDiatonic :: (HasPitches' t, Functor f, Music.Score.Pitch t ~ f Pitch) => Pitch -> DiatonicSteps -> t -> t
+upDiatonic o n = over pitches' (fmap $ upDiatonicP o n)
+
+downDiatonic :: (HasPitches' t, Functor f, Music.Score.Pitch t ~ f Pitch) => Pitch -> DiatonicSteps -> t -> t
+downDiatonic o n = over pitches' (fmap $ downDiatonicP o n)
+
+invertPitchesDiatonically :: (HasPitches' t, Functor f, Music.Score.Pitch t ~ f Pitch) => Pitch -> t -> t
+invertPitchesDiatonically o = over pitches' (fmap $ invertPitchesDiatonicallyP o)
+
+upDiatonicP :: Pitch -> DiatonicSteps -> Pitch -> Pitch
+upDiatonicP origin n = relative origin $ (_steps +~ n)
+
+downDiatonicP :: Pitch -> DiatonicSteps -> Pitch -> Pitch
+downDiatonicP origin n = relative origin $ (_steps -~ n)
+
+-- TODO mixing up reflection point and tonic?
+-- Basically, we always invert around c, adding relative only moves the tonic and inverts around c
+invertPitchesDiatonicallyP :: Pitch -> Pitch -> Pitch
+invertPitchesDiatonicallyP origin = relative origin $ (_steps %~ negate)
+
+newtype ChromaticSteps = ChromaticSteps { getChromaticSteps :: Integer }
+  deriving (Eq, Ord, Show, Enum, Num, Real, Integral)
+
+newtype DiatonicSteps = DiatonicSteps { getDiatonicSteps :: Integer }
+  deriving (Eq, Ord, Show, Enum, Num, Real, Integral)
+
+_steps :: Lens' Interval DiatonicSteps
+_steps = from interval' . _2
+
+diatonicSteps :: Iso' Number DiatonicSteps
+diatonicSteps = iso n2d d2n
+  where
+    n2d n | n > 0  = fromIntegral (n - 1)
+    n2d n | n == 0 = error "diatonicSteps: Invalid number 0"
+    n2d n | n < 0  = fromIntegral (n + 1)
+
+    d2n n | n >= 0 = fromIntegral (n + 1)
+    d2n n | n <  0 = fromIntegral (n - 1)
+
+_quality :: Lens' Interval Quality
+_quality = from interval . _1
+
+_number :: Lens' Interval Number 
+_number = from interval . _2
+
+interval :: Iso' (Quality, Number) Interval
+interval = iso (uncurry mkInterval2) (\x -> (quality x, number x))
+
+-- Interval as alteration and diatonic steps
+interval' :: Iso' (ChromaticSteps, DiatonicSteps) Interval
+interval' = iso (\(d,s) -> mkInterval' (fromIntegral d) (fromIntegral s)) 
+  (\x -> (qualityToDiff (number x >= 0) (expectedQualityType (number x)) (quality x), (number x)^.diatonicSteps))
+
+-- -- TODO be "nice"
+-- mkIntervalNice :: Quality -> Number -> Interval
+-- mkIntervalNice q n
+--   | expectedQualityType n `elem` qualityTypes q = mkInterval2 q n 
+--   | expectedQualityType n == MajorMinorType     = mkInterval2 (toMajorMinorType q) n
+--   | expectedQualityType n == PerfectType        = mkInterval2 (toPerfectType q) n
+
+toMajorMinorType Perfect = Major
+toPerfectType    Major   = Perfect
+toPerfectType    Minor   = (Diminished 1)
+-- toPerfectType    x = x
+
+-}
+
+
+{-
+
+
+p1 = scat $ take 5 $ iterate (times 3 . compress 2) $ scat [c,g,f]
+p2 = scat $ take 5 $ iterate (times 3 . compress 2) $ scat [c,g,fs,f]
+p3 = scat $ take 4 $ iterate (times 3 . compress 2) $ scat [c,ab,g,fs,f]
+p4 = scat $ take 4 $ iterate (times 3 . compress 2) $ scat [c,bb,a,ab,g]
+
+OK
+>>> :o scat $ down (_P8) $ take 8 $ iterate (upDiatonic c 1) $ pcat [c,e,g]
+NOT OK
+>>> :o scat $ up (_P8) $ take 8 $ iterate (downDiatonic c 1) $ pcat [c,e,g]
+-}
+-- t1 = open $ scat $ down (_P8) $ take 8 $ iterate (upDiatonic c 1) $ pcat [c,e,g]
+-- t2 = open $ scat $ up (_P8) $ take 8 $ iterate (downDiatonic c 1) $ pcat [c,e,g]
+
+{-
+>>> :o let x = scat [colorRed c,d,e]^/3 in (x |> scat (fmap (\n -> text (show n) $ upDiatonic c n x) [0..10]))
+
+-- TODO this one still crashes
+>>> :o let x = scat [colorRed c,d,e]^/3 in (x |> scat (fmap (\n -> text (show n) $ upDiatonic c n x) [0..10]))
+
+
+-}
+          
+{-|
+>>> m3 & _number %~ pred
+m2
+>>> m3 & _number %~ succ
+d4
+>>> _M3 & _number %~ succ
+_P4
+
+
+>>> m3 & _number +~ 1
+d4
+>>> m3 & _number +~ 2
+d5
+>>> m3 & _number +~ 3
+m6
+>>> m3 & _number +~ 4
+
+
+>>> m3 & _quality .~ Minor
+m3
+>>> _P5 & _quality .~ Minor
+d5
+>>> (-d5) & _quality %~ diminish
+
+
+TODO only obeys lens laws up to quality normalization
+
+>>> _P5 & _quality .~ Minor
+d5
+>>> _P5 & _quality .~ (Diminished 1)
+d5
+
+-}             
+
+
+
+
+
+-- Internal stuff
 
 {-
 isPerfectNumber :: Int -> Bool
