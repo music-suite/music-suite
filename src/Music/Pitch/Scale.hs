0be980c8d135057c701f0ee27693a2df08f28dda
Hans Höglund <hans@hanshoglund.se>
2014-12-01 21:13:38 +0000

    Add Pitch.Scale (sketching generic scales/modes/chords)

--- src/Music/Pitch/Scale.hs
+++ src/Music/Pitch/Scale.hs
@@ -20,31 +20,169 @@
 -------------------------------------------------------------------------------------
 
 module Music.Pitch.Scale (
+  Mode,
   Scale,
-  ScaleType,
-  Chord,
+  majorScale,
+  pureMinorScale,
+  harmonicMinorScale,
+  melodicMinorScale,
+  aeolian,
+  locrian,
+  ionian,
+  dorian,
+  phrygian,
+  lydian,
+  mixolydian,
+  majorPentaTonic,
+  minorPentaTonic,
+  bluesScale,
+  bebopScale,
+  wholeTone,
+  octatonic,
+  firstMode,
+  secondMode,
+  thirdMode,
+  fourthMode,
+  fifthMode,
+  sixthMode,
+  seventhMode,
+
   Function,
+  _intervals,
+  _repeat,
+  Chord,
+  _tonic,
+  
+  majorTriad,
+  minorTriad,
+  augmentedChord,
+  diminishedChord,
+  halfDiminishedChord,
+  majorSeventhChord,
+  
   ) where
 
 import Data.VectorSpace
 import Data.AffineSpace
-import Music.Pitch -- Test
+import Music.Pitch.Literal
+import Control.Lens
+import Music.Pitch hiding (Mode) -- Test
+
+data Mode a = Mode [Diff a] (Diff a) -- intervals, repeat (usually octave)
+data Scale a = Scale a (Mode a) -- root, mode
+
+data Function a = Function [Diff a] (Diff a) -- intervals, repeat, repeat (usually octave)
+data Chord a = Chord a (Function a) -- root, function 
+
+_intervals :: Lens' (Function a) [Diff a]
+_intervals f (Function is r) = fmap (\is -> Function is r) $ f is
+
+_repeat :: Lens' (Function a) (Diff a)
+_repeat f (Function is r) = fmap (\r -> Function is r) $ f r
+
+{-
+_pitches :: Lens' (Chord a) [a]
+_pitches f (Chord t xs) = fmap (\xs -> Chord t (fmap (.-. t) xs)) $ f (fmap (.+^ t) $ xs) . _intervals
+-}
+
+_tonic :: Lens' (Chord a) a
+_tonic f (Chord t xs) = fmap (\t -> Chord t xs) $ f t
+
+majorScale :: Mode Pitch
+majorScale = Mode [_M2,_M2] _P8
+
+pureMinorScale :: Mode Pitch
+pureMinorScale = undefined
+
+harmonicMinorScale :: Mode Pitch
+harmonicMinorScale = undefined
+
+melodicMinorScale :: Mode Pitch
+melodicMinorScale = undefined
+
+aeolian :: Mode Pitch
+aeolian = Mode [_M2,_M2,m2,_M2,_M2,m2,_M2] _P8
+
+locrian :: Mode Pitch
+locrian = undefined
+
+ionian :: Mode Pitch
+ionian = undefined
+
+dorian :: Mode Pitch
+dorian = undefined
+
+phrygian :: Mode Pitch
+phrygian = undefined
+
+lydian :: Mode Pitch
+lydian = undefined
+
+mixolydian :: Mode Pitch
+mixolydian = undefined
+
+majorPentaTonic :: Mode Pitch
+majorPentaTonic = undefined
+
+minorPentaTonic :: Mode Pitch
+minorPentaTonic = undefined
+
+bluesScale :: Mode Pitch
+bluesScale = undefined
+
+bebopScale :: Mode Pitch
+bebopScale = undefined
+
+wholeTone :: Mode Pitch
+wholeTone = undefined
+
+octatonic :: Mode Pitch
+octatonic = undefined
+
+firstMode :: Mode Pitch
+firstMode = undefined
+
+secondMode :: Mode Pitch
+secondMode = undefined
+
+thirdMode :: Mode Pitch
+thirdMode = undefined
+
+fourthMode :: Mode Pitch
+fourthMode = undefined
+
+fifthMode :: Mode Pitch
+fifthMode = undefined
+
+sixthMode :: Mode Pitch
+sixthMode = undefined
+
+seventhMode :: Mode Pitch
+seventhMode = undefined
 
 
-data ScaleType a = ScaleType [Diff a] (Diff a)
-data Scale a = Scale a (ScaleType a)
-
-data Function a = Function [Diff a] (Diff a)
-data Chord a = Chord a (Function a)
-
--- Test
-cMajorScale = Scale c majorScale
-
-majorScale :: ScaleType Pitch
-majorScale = ScaleType [_M2,_M2] _P8
 
 
-data Mode a = Mode (Scale a) a
+majorTriad :: Function Pitch
+majorTriad = undefined
 
-cMajorKey = Mode cMajorScale c
-aMajorKey = Mode cMajorScale a
+minorTriad :: Function Pitch
+minorTriad = undefined
+
+augmentedChord :: Function Pitch
+augmentedChord = undefined
+
+diminishedChord :: Function Pitch
+diminishedChord = undefined
+
+halfDiminishedChord :: Function Pitch
+halfDiminishedChord = undefined
+
+majorSeventhChord :: Function Pitch
+majorSeventhChord = undefined
+
+
+
+
+
+
