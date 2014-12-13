dbfa53b7c54b959c503a234ad3a94491abaf5254
Hans Höglund <hans@hanshoglund.se>
2014-12-13 17:56:24 +0000

    Add lenses _solo, _subpart, _instrument Also add smallestPart etc

--- src/Music/Parts.hs
+++ src/Music/Parts.hs
@@ -34,8 +34,15 @@ module Music.Parts (
 
         -- * Parts
         Part(..),
+        _solo,
+        _subpart,
+        _instrument,
         divide,
         containsPart,
+        smallestPart,
+        smallestSubpart,
+        distinctFrom,
+        allDistinct,
         solo,
         tutti,
 
@@ -139,7 +146,7 @@ module Music.Parts (
   ) where
 
 import           Control.Applicative
-import           Control.Lens                    (toListOf)
+import           Control.Lens                    (toListOf, Lens, Lens')
 import           Data.Default
 import           Data.Functor.Adjunction         (unzipR)
 import qualified Data.List
@@ -215,6 +222,58 @@ Part solo1 instr1 subp1 `containsPart` Part solo2 instr2 subp2 =
         solo1 == solo2
         && instr1 == instr2
         && subp1 `containsSubpart` subp2
+
+smallestPart :: Part -> Part -> Part
+smallestPart p1@(Part _ _ sp1) p2@(Part _ _ sp2)
+  | sp1 `smallestSubpart` sp2 == sp1 = p1
+  | sp1 `smallestSubpart` sp2 == sp2 = p2
+
+
+smallestSubpart :: Subpart -> Subpart -> Subpart
+smallestSubpart x y
+  | x `isProperSubpartOf` y = x
+  | y `isProperSubpartOf` x = y
+  -- arbitrarily:
+  | otherwise               = x
+
+
+-- | Returns 'True' iff all given parts are distinct (as per 'distinctFrom').
+allDistinct :: [Part] -> Bool
+allDistinct []     = True
+allDistinct (x:xs) = all (distinctFrom x) xs && allDistinct xs
+
+-- | Returns 'True' iff x and y are completely distinct, i.e. neither contains the other.
+-- 
+-- >>> violins `distinctFrom` trumpets
+-- True
+-- >>> violins `distinctFrom` violins
+-- False
+-- >>> violins `distinctFrom` violins1
+-- False
+-- >>> violins1 `distinctFrom` violins
+-- False
+-- >>> violins1 `distinctFrom` violins2
+-- True
+--
+distinctFrom :: Part -> Part -> Bool
+distinctFrom (Part s1 i1 sp1) (Part s2 i2 sp2) = s1 /= s2 || i1 /= i2 || noneSubpart
+  where
+    -- Is this needed?
+    noneSubpart = not (sp1 `isSubpartOf` sp2) && not (sp2 `isSubpartOf` sp1)
+
+
+    
+    -- if equal
+    -- [pa',pb'] = divide 2 pa
+
+_solo :: Lens' Part Solo
+_solo f (Part s i u) = fmap (\s -> Part s i u) $ f s
+
+_subpart :: Lens' Part Subpart
+_subpart f (Part s i u) = fmap (\u -> Part s i u) $ f u
+
+_instrument :: Lens' Part Instrument
+_instrument f (Part s i u) = fmap (\i -> Part s i u) $ f i
 
 -- | Divide a part into @n@ subparts.
 divide :: Int -> Part -> [Part]
