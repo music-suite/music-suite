a13866773e11b03532334cdfaa6f51f439613fe9
Hans Höglund <hans@hanshoglund.se>
2015-02-09 12:47:55 +0000

    new file:   sketch/Model.hs

---
+++ sketch/Model.hs
@@ -0,0 +1,22 @@
+
+type Time
+type Duration
+type Span = Time^2
+
+type Note  a = (Duration, a)
+type Event a = (Span, a)
+
+type Voice a = [Note a] -- sum monoid
+type Score a = [Event a] -- hull/composition monoid?
+
+type Aligned a = (Time, LocalDuration, a)
+
+
+type Behavior a = Time -> a
+type Reactive a = ([Time], a)
+
+-- Remove segment
+
+
+
+
