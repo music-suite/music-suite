ca40fccd806bc9cecfa26a5c511b256603055c72
Hans Höglund <hans@hanshoglund.se>
2015-03-20 17:35:11 +0000

    Stop reexporting Pitch.Common.Types

--- src/Music/Pitch/Common.hs
+++ src/Music/Pitch/Common.hs
@@ -1,7 +1,6 @@
 
 -- | Common pitch representation.
 module Music.Pitch.Common (
-    module Music.Pitch.Common.Types,
     module Music.Pitch.Common.Semitones,
     module Music.Pitch.Common.Quality,
     module Music.Pitch.Common.Number,
@@ -15,7 +14,6 @@ module Music.Pitch.Common (
 import Music.Pitch.Common.Pitch
 import Music.Pitch.Common.Quality
 import Music.Pitch.Common.Number
-import Music.Pitch.Common.Types
 import Music.Pitch.Common.Interval
 import Music.Pitch.Common.Semitones
 import Music.Pitch.Common.Spell
