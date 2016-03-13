6b1bef7e157d864ec08e39702147990d2977bbfc
Hans <hans@hanshoglund.se>
2016-03-13 01:10:45 +0000

    Examples: Generalized some types

--- examples/voices.hs
+++ examples/voices.hs
@@ -1,16 +1,16 @@
 
 import Music.Prelude
-import qualified Music.Score
+import qualified Music.Score as S
 
 subj :: Voice (Maybe Pitch)
 subj = mconcat [c',ab,db',e,f,g,ab,bb,c']
 
 type Chorale = [Voice (Maybe Pitch)]
 
-renderVoice :: Voice (Maybe Pitch) -> Score StandardNote
+renderVoice :: IsPitch a => Voice (Maybe Pitch) -> Score a
 renderVoice = fmap fromPitch'' . removeRests . renderAlignedVoice . aligned 0 0
 
-renderChorale :: Chorale -> Score StandardNote
+renderChorale :: (IsPitch a, HasParts' a, S.Part a ~ Part) => Chorale -> Score a
 renderChorale = catSep . fmap renderVoice
 
 catSep = pcat . zipWith (set parts') (divide 100 mempty)
