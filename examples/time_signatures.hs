6f4c93117f3b29ab3382a8320dd2aa4077c1490a
Hans Hoeglund <hans@hanshoglund.se>
2019-12-08 22:53:22 +0000

    Make more examples compile

--- examples/time_signatures.hs
+++ examples/time_signatures.hs
@@ -3,10 +3,8 @@
 
 import Music.Prelude
 
-main = open music
-
+music :: Music
 music = id
-
   $ title "Time signatures"
 
   -- Try commenting out some of these lines
@@ -15,5 +13,7 @@ music = id
   $ timeSignature (2/4)
   $ timeSignature ((4+3)/8)
   $ timeSignature ((3+4)/8)
-  
+
   $ scat [c,c',b,bb,a,as,g|*2,scat [f,e,d,b_]|/2,d|*2,c|*2]|/8
+
+main = pure ()
