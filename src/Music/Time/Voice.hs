0b5c662353bf03ae772a30b0ca5c7c8e6807fc96
Hans Hoeglund <hans@hanshoglund.se>
2020-01-19 18:10:44 +0000

    Remove undefined

--- src/Music/Time/Voice.hs
+++ src/Music/Time/Voice.hs
@@ -797,13 +797,10 @@ mergeIfSameDurationWith f a b
 --     dup x = (x,x)
 --
 -- splitLatterToAssureSameDurationWith :: (b -> (b, b)) -> Voice b -> Voice b -> Voice b
--- splitLatterToAssureSameDurationWith = undefined
 
 -- polyToHomophonic      :: [Voice a] -> Maybe (Voice [a])
--- polyToHomophonic = undefined
 --
 -- polyToHomophonicForce :: [Voice a] -> Voice [a]
--- polyToHomophonicForce = undefined
 
 -- | Split a homophonic texture into a polyphonic one. The returned voice list will
 -- have as many elements as the chord with the fewest number of notes.
@@ -818,16 +815,12 @@ homoToPolyphonic xs = case nvoices xs of
     maybeMinimum xs = if null xs then Nothing else Just (minimum xs)
 
 -- changeCrossing   :: Ord a => Voice a -> Voice a -> (Voice a, Voice a)
--- changeCrossing = undefined
 --
 -- changeCrossingBy :: Ord b => (a -> b) -> Voice a -> Voice a -> (Voice a, Voice a)
--- changeCrossingBy = undefined
 --
 -- processExactOverlaps :: (a -> a -> (a, a)) -> Voice a -> Voice a -> (Voice a, Voice a)
--- processExactOverlaps = undefined
 --
 -- processExactOverlaps' :: (a -> b -> Either (a,b) (b,a)) -> Voice a -> Voice b -> (Voice (Either b a), Voice (Either a b))
--- processExactOverlaps' = undefined
 
 -- | Returns the onsets of all notes in a voice given the onset of the first note.
 onsetsRelative :: Time -> Voice a -> [Time]
