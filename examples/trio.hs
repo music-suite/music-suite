8bd1f1e31a050e85a0f95eb09369f5971b4a0ac2
Hans Hoeglund <hans@hanshoglund.se>
2020-02-15 15:24:20 +0000

    Cleanup Prelude.Standard

--- examples/trio.hs
+++ examples/trio.hs
@@ -33,7 +33,7 @@ celloEntry :: Music
 celloEntry = set parts' cellos e'' |*(25*5/8)
 
 mainCanon :: Music
-mainCanon = timeSignature (time 6 8) $ asScore $
+mainCanon = timeSignature (time 6 8) $
     (set parts' violins1 $ harmonic 2 $ times 50 $ legato $ accentLast $
         octavesUp 2 $ pseq [a_,e,a,cs',cs',a,e,a_]|/8)
 
