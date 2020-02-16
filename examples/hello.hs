dc005f476f85aa2bc80a360135c4cf1845de2434
Hans Hoeglund <hans@hanshoglund.se>
2020-02-16 13:36:43 +0000

    Revert changes to Hello World example

--- examples/hello.hs
+++ examples/hello.hs
@@ -17,13 +17,7 @@ import Music.Prelude
 --
 -- The 'times' function repeats the 3-note figure 5 times.
 music :: Music
-music =
-  set (parts' . instrument) violin $
-    rcat
-      [ legato $ times 3 $ (cs |> ppar[a,g] |> e) |* (1/4)
-      , legato $ times 3 $ (cs |> ppar[a,g] |> e) |* (1/4) |* 2
-      , delay 2 $ legato $ times 3 $ (cs |> ppar[a,g] |> e) |* (1/4) |* (2/5)
-      ]
+music = times 5 $ (c |> d |> e) |* (1/4)
 
 -- This line creates a standard Haskell main function that exports the piece
 -- 'music'. This means we can run this file as a standard Haskell program,
