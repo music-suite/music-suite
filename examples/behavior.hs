77e5a3e0fe7e77e78f63fcc2be5ae233fc29dd57
Hans Hoeglund <hans@hanshoglund.se>
2020-02-19 16:10:06 +0000

    Clean up behavior example

--- examples/behavior.hs
+++ examples/behavior.hs
@@ -12,8 +12,6 @@ toPitch x = c .+^ si x
   where
     si t = spell usingSharps $ floor @_ @Semitones t
 
-music :: [Score (Behavior Pitch)]
-music = fmap (\(p, x) -> set pitches (fmap toPitch p) x) sines
 
 -- Here 'delay' and 'stretch' are used to adjust phase and frequency
 -- of a sine.
@@ -21,7 +19,11 @@ music = fmap (\(p, x) -> set pitches (fmap toPitch p) x) sines
 -- Note that numeric operators are defined pointwise on behaviors.
 -- The primitive 'sine' function has amplitude 1, so (sine * n) has
 -- amplitude n.
-
+--
+-- A 'Score ()' is used to determine the onset and offset of each
+-- note (e.g. where to sample the Behavior). Note that behaviors
+-- are defined for the entire time domain (they extend "infinitely"
+-- in both directions), so we can sample them at any point.
 sines :: [(Behavior Double, Score ())]
 sines =
   [ (delay 0 $ stretch 12 $ sine * 7, times 24 c)
@@ -36,4 +38,5 @@ at :: IsPitch a => Behavior Pitch -> a
 at = fromPitch . ($ 0) . view sampled
 
 main :: IO ()
-main = defaultMain $ rcat $ fmap (fmap at) $ music
+main = defaultMain $ rcat $ fmap (fmap at) $
+  fmap (\(p, x) -> set pitches (fmap toPitch p) x) sines
