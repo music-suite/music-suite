27f8e52d9a2cf5d3d50e7c470c9627d40637d9d9
Hans Hoeglund <hans@hanshoglund.se>
2020-04-05 13:19:02 +0100

    Add more examples

--- examples/klangfarben.hs
+++ examples/klangfarben.hs
@@ -1,7 +1,5 @@
 
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 -- This example shows a Webern style orchestration of a single voice
 -- distributed throughout several parts.
@@ -16,17 +14,9 @@ main :: IO ()
 main = defaultMain music
 
 music :: Music
-music = compress 4 $ renderAlignedVoice $ aligned 0 0 $ orchestrated
-
-orchestrated :: (HasParts' a, HasPitches' a, Music.Score.Pitch a ~ Pitch, Music.Score.Part a ~ Part, IsPitch a) => Voice a
-orchestrated = _8vb $ klangfarben
-  (cycle (vs++[violas,cellos,doubleBasses,flutes, oboes, clarinets,bassoons,horns,trumpets,trombones,tutti timpani]))
-  -- [violins,flutes,oboes,trumpets,tubas,clarinets,trombones,doubleBasses]
-  (mconcat $ [c,d,e,f,g,a,b,c',d',e',f',g',a',b',c'',d''])
-  -- (mconcat [c,e|*3,fs,g,a|/2,gs'|*3,g',fs',as,b,cs])
-
-[v1,v2,v3,v4,v5] = vs
-vs = divide 5 violins
+music = compress 4 $ renderAlignedVoice $ aligned 0 0 $ klangfarben
+  (cycle [violins,flutes,oboes,trumpets,tubas,clarinets,trombones,doubleBasses])
+  (mconcat [c,e|*3,fs,g,a|/2,gs'|*3,g',fs',as,b,cs])
 
 klangfarben :: HasParts' a => [Music.Score.Part a] -> Voice a -> Voice a
 klangfarben ps v = (^.voice) $ zipWith (set parts') ps (v^.notes)
