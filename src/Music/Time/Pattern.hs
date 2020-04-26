6c8a27961d5bb07d95097d0c1562854326dd244f
Hans Hoeglund <hans@hanshoglund.se>
2020-04-26 17:45:34 +0100

    Remove unnecessary constraints on renderPattern

--- src/Music/Time/Pattern.hs
+++ src/Music/Time/Pattern.hs
@@ -107,102 +107,6 @@ divModDur x v = (n, r)
     r = x ^-^ (v *^ fromIntegral n)
 {-# INLINE divModDur #-}
 
--- |
--- An infinitely repeating monophonic pattern.
-newtype Lunga a = Lunga (Duration, Voice a, Voice a)
-  deriving (Functor, Foldable, Traversable)
-
-instance Applicative Lunga where
-
-  pure x = Lunga (mempty, pure x, pure x)
-
-  Lunga (fd, fa, fb) <*> Lunga (xd, xa, xb) = Lunga (fd <> xd, fa <*> xa, fb <*> xb)
-
-instance Transformable (Lunga a) where
-  transform t (Lunga (d, a, b)) = Lunga (transform t d, transform t a, transform t b)
-
-type instance Pitch (Lunga a) = Pitch a
-
-type instance SetPitch b (Lunga a) = Lunga (SetPitch b a)
-
-type instance Part (Lunga a) = Part a
-
-type instance SetPart b (Lunga a) = Lunga (SetPart b a)
-
-type instance Articulation (Lunga a) = Articulation a
-
-type instance SetArticulation b (Lunga a) = Lunga (SetArticulation b a)
-
-instance HasArticulations a b => HasArticulations (Lunga a) (Lunga b) where
-  articulations = traverse . articulations
-
-instance HasPitches a b => HasPitches (Lunga a) (Lunga b) where
-  {-
-    pitches visit (Lunga (dur, back, front)) = do
-      back' <- pitches visit $ back
-      front' <- pitches visit $ front
-      pure $ Lunga (dur, front', back')
-  -}
-  pitches = traverse . pitches
-
-instance HasParts a b => HasParts (Lunga a) (Lunga b) where
-  parts = traverse . parts
-
--- nominalDuration :: Lunga a -> Duration
--- nominalDuration (Lunga (d, _, _)) = d
-
--- Transform a voice (assumed to be finite) into a lunga.
-newLunga :: Voice a -> Lunga a
-newLunga v = Lunga (v ^. duration, cycleV $ over notes reverse v, cycleV v)
-  where
-    cycleV = over notes cycle
-
-{-
-TODO write a version of renderLungaSpan'' that can handle *any* span
-  0 < on < off
-  on < 0 < off
-  on < off < 0
-
-We know that the origo of the lunga is 0. We also know the *nominal duration* d.
-Hence we know that the repeating pattern starts and ends at [-nd..-1d,0,1d,2d..nd].
-When requested to render a span s, we want to use *as short voices as possible*.
-  If s starts at nd+0 where n is integral, simply take (s^.duration) from the front voice.
-  If s starts at nd+m where n is integral and 0 < m < d,
-    EITHER
-      take d-m from the back voice, put together with the result of taking ((s^.duration) + m - d) from the front voice.
-    OR
-      drop m from the front voice, then take (d-m), put together...
-
->>> (1::Time) `cycleAndPhase` 3
-(0,1)
->>> (7::Time) `cycleAndPhase` 3
-(2,1)
-
-Render the cycles 0<->3, 3<->6, an extra cycle of (takeM 1), finally drop 1
-
->>>
->>> (1::Time) `cycleAndPhase` 3
-(0,1)
->>> (2::Time) `cycleAndPhase` 3
-(0,2)
-Render the cycles -, an extra cycle of (takeM 2), finally drop 1
-
--}
-
-
-
-
-renderLunga :: Span -> Lunga a -> Aligned (Voice a)
-renderLunga s (Lunga (d, _, b))
-  | d < 0 = error "renderLunga: Negative (nominal) duration"
-  | otherwise =
-    aligned (s ^. position 0) 0 $ voca
-  where
-    (n1, r1) = (s ^. onset) `cycleAndPhase` d
-    (n2, r2) = (s ^. offset) `cycleAndPhase` d
-    fullCycles = fromIntegral $ n2 - n1 -- 0 or greater
-    voca = dropM r1 $ takeM (d ^* fullCycles) b <> takeM r2 b
-
 
 -- TODO move Abort elsewhere
 
@@ -336,11 +240,13 @@ testSplitAt = all id
 modDur :: Duration -> Duration -> Duration
 modDur a b = snd $ a `divModDur` b
 
-getCycles :: Span -> Span -> (Duration, Integer, Duration)
+getCycles :: Span -> Span -> Either (Duration, Duration) (Duration, Integer, Duration)
 getCycles ts s
   | _duration ts <= 0 = error "getCycles: Duration of repeated span must be >0"
   --  | _duration s < _duration ts = (0, 0, _duration s)
-  | otherwise = (rem, cycles, phase)
+  | otherwise = if cycles >= 0
+      then Right (rem, cycles, phase)
+      else Left (_duration ts - rem, 0)
   where
     phase = (view offset s .-. view offset ts) `modDur` _duration ts
     (cycles, rem) = (_duration s - phase) `divModDur` _duration ts
@@ -348,15 +254,24 @@ getCycles ts s
 -- TODO proper test
 testGetCycles :: Bool
 testGetCycles = all id
-  [ getCycles (0 <-> 2) (0 <-> 4)         == (0, 2, 0)
-  , getCycles (0 <-> 2) (1 <-> 4)         == (1, 1, 0)
-  , getCycles (0 <-> 2) ((-1) <-> 5)      == (1, 2, 1)
-  , getCycles (0 <-> 2) ((-9) <-> (-2.5)) == (1, 2, 1.5)
-  , getCycles (0 <-> 2) (2 <-> 4)         == (0, 1, 0)
-  , getCycles (1 <-> 3) (1 <-> 2)         == (0, 0, 1)
-  , getCycles (1 <-> 3) (2 <-> 3)         == (1, 0, 0)
-  , getCycles (1 <-> 3) (0 <-> 1)         == (1, 0, 0)
-  , getCycles (1 <-> 3) ((-1) <-> 0)      == (0, 0, 1)
+  -- At least one cycle
+  [ getCycles (0 <-> 2) (0 <-> 4)         == Right (0, 2, 0)
+  , getCycles (0 <-> 2) (1 <-> 4)         == Right (1, 1, 0)
+  , getCycles (0 <-> 2) ((-1) <-> 5)      == Right (1, 2, 1)
+  , getCycles (0 <-> 2) ((-9) <-> (-2.5)) == Right (1, 2, 1.5)
+  , getCycles (0 <-> 2) (2 <-> 4)         == Right (0, 1, 0)
+
+  -- No full cycles, s properly enclosed by a cycle
+  -- TODO second pair component is useless, remove!
+  , getCycles (0 <-> 2) (0.5 <-> 1.5)     == Left  (0.5, 0)
+  , getCycles (0 <-> 2) (0.6 <-> 1.6)     == Left  (0.6, 0)
+  , getCycles (0 <-> 2) ((-0.2) <-> (-0.1)) == Left (1.8, 0)
+
+  -- No full cycles, s NOT properly enclosed by a cycle
+  , getCycles (1 <-> 3) (1 <-> 2)         == Right (0, 0, 1)
+  , getCycles (1 <-> 3) (2 <-> 3)         == Right (1, 0, 0)
+  , getCycles (1 <-> 3) (0 <-> 1)         == Right (1, 0, 0)
+  , getCycles (1 <-> 3) ((-1) <-> 0)      == Right (0, 0, 1)
   ]
 
 -- TODO get dir of (newtype wrapper, or inline):
@@ -365,7 +280,7 @@ instance (HasDuration a, Transformable a) => HasPosition [Aligned a] where
     EmptyInterval -> Nothing
     NonEmptyInterval x -> Just x
 
-testC :: Span -> Aligned (Voice a) -> (Duration, Integer, Duration)
+testC :: Span -> Aligned (Voice a) -> Either (Duration, Duration) (Duration, Integer, Duration)
 testC s l = getCycles (_era1 l) s
 
 testRenderLungaNEW :: Bool
@@ -385,9 +300,11 @@ testRenderLungaNEW = all id
   , renderLungaNEW ((0)<->1) aba1
   == [(0 <-> 1,'c')^.event]^.score
 
+  , renderLungaNEW ((0.5)<->1.5) (aligned 0 0 $ stretch 2 $ pure 'c')
+  == [(0.5 <-> 1.5,'c')^.event]^.score
+
   , renderLungaNEW (3<->8) aba
   == [(3 <-> 4,'c')^.event,(4 <-> 5,'a')^.event,(5 <-> 7,'b')^.event,(7 <-> 8,'c')^.event]^.score
-
 
   , renderLungaNEW (0<->2) (aligned 0 0 c :: Aligned (Voice Int))
   == [(0 <-> 1,0)^.event,(1 <-> 2,0)^.event]^.score
@@ -409,23 +326,24 @@ testRenderLungaNEW = all id
 
 -- TODO replace the old ones, change type of pattern to [Aligned (Voice a)]
 renderLungaNEW :: Span -> Aligned (Voice a) -> Score a
-renderLungaNEW s l =
-  -- FIXME if too short, must take phase into account!
-  mconcat $ fmap renderAlignedVoice $ [intro] ++ cycles ++ [outro]
+renderLungaNEW s l = case getCycles (_era1 l) s of
+  Left (phase, _) ->
+    renderAlignedVoice $ alignOnsetTo (view onset s)
+      (takeM (_duration s) $ dropM phase $ unAlign l)
+  Right (introDur, numCycles, outroDur) -> let
+      intro  = alignOnsetTo (view onset s) (dropM (_duration l - introDur) $ unAlign l)
+      outro  = alignOffsetTo (view offset s) (takeM outroDur $ unAlign l)
+      cycles = times (fromIntegral numCycles) $
+              pure $ alignOnsetTo (view onset s .+^ introDur) (unAlign l)
+    in mconcat $ fmap renderAlignedVoice $ [intro] ++ cycles ++ [outro]
   where
-    intro  = alignOnsetTo (view onset s) (dropM (_duration l - introDur) $ unAlign l)
-    outro  = alignOffsetTo (view offset s) (takeM outroDur $ unAlign l)
-    cycles = times (fromIntegral numCycles) $
-      pure $ alignOnsetTo (view onset s .+^ introDur) (unAlign l)
-    (introDur, numCycles, outroDur) = getCycles (_era1 l) s
-
     alignOnsetTo t = aligned t 0
     alignOffsetTo t = aligned t 1
 
 -- List of repeated voices
 -- TODO is this isomorphic to Tidal's pattern (i.e. Span -> Score a)
 newtype Pattern a
-  = Pattern {getPattern :: [Placed (Lunga a)]} -- origo, pattern
+  = Pattern {getPattern :: [Aligned (Voice a)]}
   deriving (Semigroup, Monoid, Transformable, Functor, Foldable, Traversable)
 
 
@@ -434,14 +352,14 @@ type instance Articulation (Pattern a) = Articulation a
 type instance SetArticulation b (Pattern a) = Pattern (SetArticulation b a)
 
 instance HasArticulations a b => HasArticulations (Pattern a) (Pattern b) where
-  articulations = iso getPattern Pattern . articulations
+  articulations = traverse . articulations
 
 type instance Pitch (Pattern a) = Pitch a
 
 type instance SetPitch b (Pattern a) = Pattern (SetPitch b a)
 
 instance HasPitches a b => HasPitches (Pattern a) (Pattern b) where
-  pitches = iso getPattern Pattern . pitches
+  pitches = traverse . pitches
 
 type instance Part (Pattern a) = Part a
 
@@ -459,23 +377,15 @@ pureP :: a -> Pattern a
 pureP = newPattern . pure
 
 newPattern :: Voice a -> Pattern a
-newPattern v = Pattern [pure $ newLunga v]
+newPattern v = Pattern [aligned 0 0 v]
 
 rhythmPattern :: IsPitch a => [Duration] -> Pattern a
 rhythmPattern a = newPattern $ fmap (const c) $ a ^. durationsAsVoice
 
 -- TODO variant that returns [Aligned (Voice a)]
 renderPattern :: Pattern a -> Span -> Score a
-renderPattern (Pattern ((unzip . fmap (^. from placed)) -> (origos, lungas))) s =
-  mconcat $
-    zipWith (renderLunga' s) origos lungas
+renderPattern (Pattern xs) s = mconcat $ fmap (renderLungaNEW s) xs
 
-renderLunga' :: Span -> Time -> Lunga a -> Score a
-renderLunga' s t = renderAlignedVoice . delay' t . renderLunga (delay' t s)
-  where
-    -- TODO terminology here is not super-nice
-    -- Work out whether this or renderLunga is the semantic function of lunga
-    delay' t = delay (0 .-. t)
 
 -- |
 -- Renders each pattern over one cycle (0<->1) and stretches to the duration of the surrounding note.
