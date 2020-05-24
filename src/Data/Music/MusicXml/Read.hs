5a1b6ac624dfcb2b19490c705acb19f1edb36a89
Joseph Morag <jm4157@columbia.edu>
2020-05-24 11:00:55 -0400

    Refactor tryAll into dispatch for sum type parsers

--- src/Data/Music/MusicXml/Read.hs
+++ src/Data/Music/MusicXml/Read.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
 
 -- |
 -- Copyright   : (c) Joseph Morag 2020
@@ -16,6 +16,7 @@ module Data.Music.MusicXml.Read
 where
 
 import Control.Applicative
+import Control.Category ((>>>))
 import Control.Monad
 import Data.Char (toUpper)
 import Data.Default
@@ -24,7 +25,6 @@ import Data.Maybe
 import Data.Music.MusicXml.Score
 import Text.Read
 import Text.XML.Light
-import TypeUnary.Nat
 
 parseScore :: Element -> Maybe Score
 parseScore top = case getName top of
@@ -34,7 +34,7 @@ parseScore top = case getName top of
             attr "version" top >>= traverse readMaybe . wordsBy (== '.')
     scoreHeader <- parseHeader top
     Partwise (ScoreAttrs scoreAttrs) scoreHeader
-      <$> traverse parsePart (children ["part"] top)
+      <$> traverse parsePart (children "part" top)
   "score-timewise" -> error "timewise not supported"
   _ -> empty
 
@@ -53,14 +53,12 @@ parseHeader top =
     movementTitle = child "movement-title" top >>= getText
     identification = do
       clist <- child "identification" top
-      Identification <$> traverse parseCreator (children ["creator"] clist)
+      Identification <$> traverse parseCreator (children "creator" clist)
     parseCreator c = Creator <$> attr "type" c <*> getText c
     partList = do
       plist <- child "part-list" top
       PartList
-        <$> traverse
-          (\p -> parsePartGroup p <|> parseScorePart p)
-          (children ["part-group", "score-part"] plist)
+        <$> dispatch [("part-group", parsePartGroup), ("score-part", parseScorePart)] plist
 
 parseScorePart :: Element -> Maybe PartListElem
 parseScorePart scorePart = do
@@ -95,98 +93,91 @@ parsePart :: Element -> Maybe (PartAttrs, [(MeasureAttrs, Music)])
 parsePart part =
   (,)
     <$> (PartAttrs <$> attr "id" part)
-    <*> traverse parseMeasure (children ["measure"] part)
+    <*> traverse parseMeasure (children "measure" part)
 
 parseMeasure :: Element -> Maybe (MeasureAttrs, Music)
 parseMeasure measure =
   (,)
     <$> (MeasureAttrs <$> (attr "number" measure >>= readMaybe))
-    <*> (Music <$> traverse parseMusicElem (children supportedElems measure))
+    <*> (Music <$> dispatch supportedElems measure)
 
-supportedElems :: [String]
+supportedElems :: [(String, Element -> Maybe MusicElem)]
 supportedElems =
-  [ "attributes",
-    "backup",
-    "forward",
-    "note",
-    "direction",
-    "barline"
+  [ ("attributes", fmap MusicAttributes . dispatch supportedAttributes),
+    ("backup", fmap MusicBackup . parseDuration),
+    ("forward", fmap MusicForward . parseDuration),
+    ("note", fmap MusicNote . parseNote),
+    ("direction", fmap MusicDirection . parseDirection),
+    ("barline", fmap MusicBarline . parseBarline)
   ]
 
-elemParsers :: [Element -> Maybe MusicElem]
-elemParsers =
-  [ fmap MusicAttributes . parseAttributes,
-    fmap MusicBackup . parseDuration,
-    fmap MusicForward . parseDuration,
-    fmap MusicNote . parseNote,
-    fmap MusicDirection . parseDirection,
-    fmap MusicBarline . parseBarline
+supportedAttributes :: [(String, Element -> Maybe Attributes)]
+supportedAttributes =
+  [ ("divisions", parseDivisions),
+    ("key", parseKey),
+    ("time", parseTime),
+    ("instruments", readText >=> pure . Instruments),
+    ("clef", parseClef),
+    ("transpose", parseTranspose)
   ]
 
-parseAttributes :: Element -> Maybe [Attributes]
-parseAttributes =
-  traverse parseAttr
-    . children
-      ["divisions", "key", "time", "instruments", "clef", "transpose"]
-  where
-    parseAttr =
-      tryAll
-        [ parseDivisions,
-          parseKey,
-          parseTime,
-          child "instruments" >=> readText >=> fmap Instruments,
-          parseClef,
-          parseTranspose
-        ]
-      where
-        parseDivisions = getText >=> readMaybe >=> fmap (Divisions . Divs)
-        parseKey key =
-          let fifths = child "fifths" key >>= readText
-              mode = case child "mode" key of
-                Nothing -> Just NoMode
-                Just m -> getText m >>= \case
-                  "nomode" -> Just NoMode
-                  (c : cs) -> readMaybe (toUpper c : cs)
-                  _ -> Nothing
-           in Key <$> (Fifths <$> fifths) <*> mode
-        parseTime time = do
-          let symbol = attr "symbol" time
-          numerator <- child "beats" time >>= readText
-          denomenator <- child "beat-type" time >>= readText
-          pure $ case (symbol, numerator, denomenator) of
-            (Just "common", 4, 4) -> Time CommonTime
-            (Just "cut", 2, 2) -> Time CutTime
-            _ -> Time $ DivTime (Beat numerator) (BeatType denomenator)
-        parseClef clef = do
-          sign <- child "sign" clef >>= getText >>= \case
-            "G" -> pure GClef
-            "C" -> pure CClef
-            "F" -> pure FClef
-            "percussion" -> pure PercClef
-            "TAB" -> pure TabClef
-            _ -> empty
-          line <- case child "line" clef of
-            -- Percussion clefs can have no line element in which case they appear
-            -- in the middle of the staff
-            Nothing -> pure 3
-            Just l -> getText l >>= readMaybe
-          let octaveChange = child "clef-octave-change" clef >>= readText
-          if line <= 5 && line >= 1
-            then pure $ Clef sign (Line line) (OctaveChange <$> octaveChange)
-            else Nothing
-        parseTranspose t =
-          Transpose
-            <$> (child "diatonic" t >>= readText)
-            <*> (child "chromatic" t >>= readText)
-            <*> (pure $ child "octave-change" t >>= readText >>= fmap OctaveChange)
+parseDivisions :: Element -> Maybe Attributes
+parseDivisions = getText >=> readMaybe >=> pure . (Divisions . Divs)
+
+parseKey :: Element -> Maybe Attributes
+parseKey key =
+  let fifths = child "fifths" key >>= readText
+      mode = case child "mode" key of
+        Nothing -> Just NoMode
+        Just m -> getText m >>= \case
+          "nomode" -> Just NoMode
+          (c : cs) -> readMaybe (toUpper c : cs)
+          _ -> Nothing
+   in Key <$> (Fifths <$> fifths) <*> mode
+
+parseTime :: Element -> Maybe Attributes
+parseTime time = do
+  let symbol = attr "symbol" time
+  numerator <- child "beats" time >>= readText
+  denomenator <- child "beat-type" time >>= readText
+  pure $ case (symbol, numerator, denomenator) of
+    (Just "common", 4, 4) -> Time CommonTime
+    (Just "cut", 2, 2) -> Time CutTime
+    _ -> Time $ DivTime (Beat numerator) (BeatType denomenator)
+
+parseClef :: Element -> Maybe Attributes
+parseClef clef = do
+  sign <- child "sign" clef >>= getText >>= \case
+    "G" -> pure GClef
+    "C" -> pure CClef
+    "F" -> pure FClef
+    "percussion" -> pure PercClef
+    "TAB" -> pure TabClef
+    _ -> empty
+  line <- case child "line" clef of
+    -- Percussion clefs can have no line element in which case they appear
+    -- in the middle of the staff
+    Nothing -> pure 3
+    Just l -> getText l >>= readMaybe
+  let octaveChange = child "clef-octave-change" clef >>= readText
+  if line <= 5 && line >= 1
+    then pure $ Clef sign (Line line) (OctaveChange <$> octaveChange)
+    else Nothing
+
+parseTranspose :: Element -> Maybe Attributes
+parseTranspose t =
+  Transpose
+    <$> (child "diatonic" t >>= readText)
+    <*> (child "chromatic" t >>= readText)
+    <*> (pure $ child "octave-change" t >>= readText >>= pure . OctaveChange)
 
 parseDuration :: Element -> Maybe Duration
-parseDuration = child "duration" >=> readText >=> fmap Divs
+parseDuration = child "duration" >=> readText >=> pure . Divs
 
 parseNote :: Element -> Maybe Note
 parseNote note = do
   fullNote <- parseFullNote note
-  let ties = fromMaybe [] $ traverse parseStartStop (children ["tie"] note)
+  let ties = fromMaybe [] $ traverse parseStartStop (children "tie" note)
   noteProps <- parseNoteProps note
   ((GraceNote fullNote ties noteProps) <$ child "grace" note)
     <|> ( do
@@ -204,14 +195,14 @@ parseFullNote note =
     parseDisplayPitch restOrUnpitched =
       pure $
         (,) <$> (child "display-step" restOrUnpitched >>= readText)
-          <*> (child "display-octave" restOrUnpitched >>= readText >>= fmap Octaves)
+          <*> (child "display-octave" restOrUnpitched >>= readText >>= pure . Octaves)
 
 parsePitch :: Element -> Maybe Pitch
 parsePitch note = do
   pitch <- child "pitch" note
   (,,) <$> (child "step" pitch >>= readText)
-    <*> (pure $ child "alter" pitch >>= readText >>= fmap Semitones)
-    <*> (child "octave" pitch >>= readText >>= fmap Octaves)
+    <*> (pure . fmap Semitones $ child "alter" pitch >>= readText)
+    <*> (fmap Octaves $ child "octave" pitch >>= readText)
 
 isChord :: Element -> IsChord
 isChord note = isJust $ child "chord" note
@@ -221,7 +212,7 @@ parseNoteProps note = do
   let noteInstrument = child "instrument" note >>= attr "id"
       noteVoice = child "voice" note >>= readText
       noteType = child "type" note >>= parseNoteType
-      noteDots = fromIntegral $ length $ children ["dot"] note
+      noteDots = fromIntegral $ length $ children "dot" note
       noteAccidental = do
         accidentalElem <- child "accidental" note
         a <- parseAccidental accidentalElem
@@ -279,9 +270,75 @@ parseNoteProps note = do
           Just "backward hook" -> pure BackwardHook
           _ -> empty
         pure (level, beamType)
-      noteNotations = []
+      noteNotations =
+        fromMaybe [] $
+          child "notations" note >>= dispatch supportedNotations
       noteLyrics = []
   pure $ NoteProps {..}
+
+supportedNotations :: [(String, Element -> Maybe Notation)]
+supportedNotations =
+  [ ("tied", fmap Tied . parseStartStopContinue),
+    ("slur", \slur -> Slur (parseLevel slur) <$> parseStartStopContinue slur),
+    ("tuplet", \tuplet -> Tuplet (parseLevel tuplet) <$> parseStartStop tuplet),
+    ("glissando", shift Glissando),
+    ("slide", shift Slide),
+    ("ornaments", const (pure $ Ornaments [])),
+    ("technical", const (pure $ Technical [])),
+    ("articulations", const (pure $ Articulations [])),
+    ("dynamics", fmap DynamicNotation . parseDynamics),
+    ( "fermata",
+      fmap Fermata
+        . ( getText >>> \case
+              Just "normal" -> pure NormalFermata
+              Just "angled" -> pure AngledFermata
+              Just "square" -> pure SquaredFermata
+              Just _ -> empty
+              Nothing -> pure NormalFermata
+          )
+    ),
+    ("non-arpeggiate", const (pure NonArpeggiate)),
+    ("arpeggiate", const (pure Arpeggiate)),
+    ("accidental-mark", fmap AccidentalMark . parseAccidental),
+    ("other-notation", fmap OtherNotation . getText)
+  ]
+  where
+    shift constructor slideType =
+      constructor (parseLevel slideType)
+        <$> parseStartStopContinue slideType
+        <*> parseLineType slideType
+        <*> pure (getText slideType)
+    parseLineType = child "line-type" >=> getText >=> \case
+      "solid" -> pure Solid
+      "dashed" -> pure Dashed
+      "dotted" -> pure Dotted
+      "wavy" -> pure Wavy
+      _ -> empty
+
+parseDynamics :: Element -> Maybe Dynamics
+parseDynamics = elChildren >>> headMay >=> getName >>> map toUpper >>> readMaybe
+
+parseNotation :: Element -> Maybe Notation
+parseNotation n =
+  fromMaybe
+    (const Nothing)
+    (lookup (getName n) supportedNotations)
+    $ n
+
+supportedOrnaments = []
+
+supportedTechniques = []
+
+supportedArticulations = []
+
+parseTechnical :: Element -> Maybe Technical
+parseTechnical = const Nothing
+
+parseOrnament :: Element -> Maybe (Ornament, [Accidental])
+parseOrnament = const Nothing
+
+parseArticulation :: Element -> Maybe Articulation
+parseArticulation = const Nothing
 
 parseNoteType :: Element -> Maybe NoteType
 parseNoteType typ = do
@@ -318,39 +375,33 @@ parseAccidental = getText >=> \case
   -- TODO quarter tone accidentals
   _ -> empty
 
--- | Total, will default level to 1
--- Subtracts 1 from "number" attribute in order to make musicxml level
--- fully representable by Max8 type. See
--- http://usermanuals.musicxml.com/MusicXML/MusicXML.htm#ST-MusicXML-beam-level.htm
 parseLevel :: Element -> Level
-parseLevel e = Level . coerceToIndex @Int . pred $ case attr "number" e >>= readMaybe of
-  Nothing -> 0
-  Just n | n < 8 && n >= 0 -> n
-  _ -> 0
+parseLevel e = maybe def mkLevel $ attr "number" e >>= readMaybe
+
+supportedDirections :: [(String, Element -> Maybe Direction)]
+supportedDirections =
+  [ ("rehearsal", fmap Rehearsal . getText),
+    ("segno", const (pure Segno)),
+    ("words", fmap Words . getText),
+    ("coda", const (pure Coda)),
+    ("dynamics", fmap Dynamics . parseDynamics),
+    ("dashes", \dashes -> Dashes (parseLevel dashes) <$> (parseStartStop dashes)),
+    ( "pedal",
+      fmap Pedal
+        . ( attr "type" >=> \case
+              "start" -> pure Start
+              "stop" -> pure Stop
+              "change" -> pure Change
+              "continue" -> pure Continue
+              _ -> empty
+          )
+    ),
+    ("other-direction", fmap OtherDirection . getText)
+    -- TODO metronome, crescendo, diminuendo
+  ]
 
 parseDirection :: Element -> Maybe Direction
-parseDirection =
-  child "direction-type"
-    >=> tryAll
-      [ fmap Rehearsal . (child "rehearsal" >=> getText),
-        fmap (const Segno) . (child "segno"),
-        fmap Words . (child "words" >=> getText),
-        fmap (const Coda) . (child "coda"),
-        -- TODO crescendo/diminuendo
-        fmap Dynamics . (child "dynamics" >=> (readMaybe . map toUpper . getName)),
-        child "dashes" >=> \dashes ->
-          Dashes (parseLevel dashes) <$> (parseStartStop dashes),
-        fmap Pedal
-          . ( child "pedal" >=> attr "type" >=> \case
-                "start" -> pure Start
-                "stop" -> pure Stop
-                "change" -> pure Change
-                "continue" -> pure Continue
-                _ -> empty
-            ),
-        -- TODO metronome
-        fmap OtherDirection . (child "other-direction" >=> getText)
-      ]
+parseDirection = child "direction-type" >=> dispatch supportedDirections >=> headMay
 
 parseBarline :: Element -> Maybe Barline
 parseBarline barline = do
@@ -381,22 +432,40 @@ parseBarline barline = do
         _ -> empty
   pure $ Barline loc style repeat
 
-parseMusicElem :: Element -> Maybe MusicElem
-parseMusicElem = tryAll elemParsers
-
 parseStartStop :: Element -> Maybe StartStop
 parseStartStop = attr "type" >=> \case
   "start" -> pure Start
   "stop" -> pure Stop
   _ -> empty
 
+parseStartStopContinue :: Element -> Maybe StartStopContinue
+parseStartStopContinue = attr "type" >=> \case
+  "start" -> pure Start
+  "stop" -> pure Stop
+  "continue" -> pure Continue
+  _ -> empty
+
 -- Run all parsers on a single element and return the successful parse
 tryAll :: (Alternative f, Foldable t) => t (a -> f b) -> a -> f b
 tryAll fs x = foldr (\fun acc -> acc <|> fun x) empty fs
 
+-- For elements with many children corresponding to a sum type. This parser
+-- is much more permissive than the other parsers in that it will simply
+-- ignore invalid elements instead of failing the whole thing
+dispatch :: [(String, Element -> Maybe a)] -> Element -> Maybe [a]
+dispatch parsers parent =
+  pure $
+    mapMaybe
+      (\e -> fromMaybe (const Nothing) (lookup (getName e) parsers) $ e)
+      (elChildren parent)
+
 -- ----------------------------------------------------------------------------------
 -- Xml utils
 -- ----------------------------------------------------------------------------------
+headMay :: [a] -> Maybe a
+headMay = \case
+  [] -> Nothing
+  x : _ -> Just x
 
 readText :: Read a => Element -> Maybe a
 readText = getText >=> readMaybe
@@ -413,8 +482,13 @@ getName = qName . elName
 child :: String -> Element -> Maybe Element
 child str = filterChildName (\q -> qName q == str)
 
-children :: [String] -> Element -> [Element]
-children strs = filterChildrenName (\q -> qName q `elem` strs)
+children :: String -> Element -> [Element]
+children str = filterChildrenName (\q -> qName q == str)
 
 attr :: String -> Element -> Maybe String
 attr str = findAttrBy (\q -> qName q == str)
+
+-- >>> xml <- parseXMLDoc <$> readFile "/home/joseph/Projects/opensheetmusicdisplay/test/data/Dichterliebe01.xml"-- <interactive>:36914:2: warning: [-Wname-shadowing]
+--     This binding for ‘xml’ shadows the existing binding
+--       defined at <interactive>:36913:2
+-- >>> xml >>= parseScore
