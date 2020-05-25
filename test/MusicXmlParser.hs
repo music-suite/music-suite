789f162a5845d641bc183ae599772c708d68a28d
Joseph Morag <jm4157@columbia.edu>
2020-05-25 15:19:43 -0400

    Use decodeUtf8 instead of Prelude.readFile

--- test/MusicXmlParser.hs
+++ test/MusicXmlParser.hs
@@ -1,5 +1,8 @@
 import Data.Maybe
 import Data.Music.MusicXml
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Encoding
 import System.Directory
 import System.FilePath
 import Test.Tasty
@@ -16,5 +19,5 @@ main = do
 
 xmlTest :: FilePath -> IO TestTree
 xmlTest file = do
-  contents <- readFile file
+  contents <- T.unpack . decodeUtf8 <$> B.readFile file
   pure $ testCase file $ assertBool "" (isJust $ readXml contents)
