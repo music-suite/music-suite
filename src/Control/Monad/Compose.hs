ac9d0b700205f0c17d38933393a92b7deeb35cb6
Hans Hoeglund <hans@hanshoglund.se>
2020-10-24 22:45:49 +0100

    Fix compiler warnings

--- src/Control/Monad/Compose.hs
+++ src/Control/Monad/Compose.hs
@@ -1,3 +1,11 @@
+{-# OPTIONS_GHC -Wall
+  -Wcompat
+  -Wincomplete-record-updates
+  -Wincomplete-uni-patterns
+  -Werror
+  -fno-warn-name-shadowing
+  -fno-warn-unused-matches
+  -fno-warn-unused-imports #-}
 -- |
 -- Monadic composition.
 --
@@ -29,8 +37,8 @@ import Data.Typeable
 --  Folds outer level to get             m (n (n a))
 --  Folds inner level to get             m (n a)
 
-mjoin :: (Monad m, Monad n, Functor m, Traversable n) => m (n (m (n a))) -> m (n a)
+mjoin :: (Monad m, Monad n, Traversable n) => m (n (m (n a))) -> m (n a)
 mjoin = fmap join . join . fmap sequence
 
-mbind :: (Monad m, Monad n, Functor m, Traversable n) => (a -> m (n b)) -> m (n a) -> m (n b)
+mbind :: (Monad m, Monad n, Traversable n) => (a -> m (n b)) -> m (n a) -> m (n b)
 mbind = (join .) . fmap . (fmap join .) . traverse
