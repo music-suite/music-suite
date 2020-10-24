ea205c566510adb4eeee9199663781658dc151fc
Hans Hoeglund <hans@hanshoglund.se>
2020-10-24 22:46:31 +0100

    Fix compiler warnings

--- src/Data/Functor/Context.hs
+++ src/Data/Functor/Context.hs
@@ -1,3 +1,11 @@
+{-# OPTIONS_GHC -Wall
+  -Wcompat
+  -Wincomplete-record-updates
+  -Wincomplete-uni-patterns
+  -Werror
+  -fno-warn-name-shadowing
+  -fno-warn-unused-matches
+  -fno-warn-unused-imports #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
