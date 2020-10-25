fe7782aba5986dae5dc34460ef85515eeb96c52a
Hans Hoeglund <hans@hanshoglund.se>
2020-10-25 13:17:14 +0000

    Fix compiler warnings

--- src/Music/Score/Internal/Export.hs
+++ src/Music/Score/Internal/Export.hs
@@ -3,14 +3,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# OPTIONS_GHC -Wall
-  -Wcompat
-  -Wincomplete-record-updates
-  -Wincomplete-uni-patterns
-  -Werror
+{-# OPTIONS_GHC
   -fno-warn-name-shadowing
-  -fno-warn-unused-matches
-  -fno-warn-unused-imports #-}
+  -fno-warn-unused-imports
+  -fno-warn-redundant-constraints #-}
 {-# OPTIONS_GHC -Wwarn #-}
 {-# OPTIONS_HADDOCK hide #-}
 
