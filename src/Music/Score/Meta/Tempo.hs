d44b81ee7ecb5e8d720c41275660f030ee5690ba
Hans Hoeglund <hans@hanshoglund.se>
2021-01-15 19:07:24 +0000

    Cleanup

--- src/Music/Score/Meta/Tempo.hs
+++ src/Music/Score/Meta/Tempo.hs
@@ -1,12 +1,3 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC
   -fno-warn-name-shadowing
   -fno-warn-unused-imports
