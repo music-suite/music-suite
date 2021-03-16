f6030d265c8ef0d6e3108d8e26036220cabc8d3e
Hans Hoeglund <hans@hanshoglund.se>
2021-03-16 19:48:26 +0000

    Move dynamics design

--- src/Music/Dynamics.hs
+++ src/Music/Dynamics.hs
@@ -23,21 +23,3 @@ import Music.Dynamics.Absolute
 import Music.Dynamics.Common
 import Music.Dynamics.Literal
 
-{-
-    ppp -42    -36
-    pp  -36    -36
-    p   -30    -24
-    mp  ?
-    mf  -24    -18
-    f   -18    -12
-    ff  -18    -12
-
-
-
-
-    Midi velocities according to Apple:
-        16  32  48  64  80  96  112  127
-        ppp pp  p   mp  mf  f   ff   fff
-
-    Nakamura (1987) The communication of dynamics between musicians and listeners through musical performance
--}
