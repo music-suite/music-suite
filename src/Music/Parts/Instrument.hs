1856fece1152bd650449ff46bc2a4498d7a12dde
Hans Hoeglund <hans@hanshoglund.se>
2022-01-03 21:32:46 +0000

    Parts: Document fromMidiProgram behavior w.r.t. playing technique

--- src/Music/Parts/Instrument.hs
+++ src/Music/Parts/Instrument.hs
@@ -107,17 +107,23 @@ instance FromJSON Instrument where
       _ -> empty
   parseJSON _ = empty
 
--- | Create an instrument from a MIDI program number.
--- Given number should be in the range 0 - 127.
+-- | Create an instrument from a General MIDI Instrument ID, in the range @0 - 127@.
 --
 -- >>> fromMidiProgram 20
 -- Reed Organ
 --
+-- Note that @Instrument@ models real-life instruments not General MIDI
+-- Instrument IDs.  For the most part these are identical, so @fromMidiProgram@
+-- and @toMidiProgram@ are inverses.
+--
 -- >>> fmap (toMidiProgram . fromMidiProgram) [20..26]
 -- [Just 20,Just 21,Just 22,Just 23,Just 24,Just 25,Just 26]
 --
--- >>> fmap (toMidiProgram . fromMidiProgram) [20..30]
--- [Just 20,Just 21,Just 22,Just 23,Just 24,Just 25,Just 26,Just 27,Just 28,Just 29,Just 30]
+-- A notable exception is the programs 26-31, which represent the same
+-- instrument (Electric Guitar), using different playing techniques.
+--
+-- >>> fmap (toMidiProgram . fromMidiProgram) [26..31]
+-- [Just 26,Just 26,Just 26,Just 26,Just 26,Just 26]
 --
 -- >>> Data.List.nub (fmap fromMidiProgram [20..26])
 -- [Reed Organ,Accordion,Harmonica,Bandoneon,Acoustic Guitar,Steel-String Guitar,Electric Guitar]
