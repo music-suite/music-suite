57ef202ceeeff216f604cdc077cc0120c85840c1
Hans Hoeglund <hans@hanshoglund.se>
2022-01-22 11:30:33 +0000

    scripts/format: Remove remaining mentions of CPP
    Also allow passing (MODE=check scripts/format.sh)
    
    Fixes #81

--- scripts/format.sh
+++ scripts/format.sh
@@ -8,6 +8,7 @@ export LANG="C.UTF-8"
 # explicitly. If this list becomes long, think of a better way
 # of coordinating extensions between Cabal, doctester and Ormulo.
 OR=ormolu\ --ghc-opt=-XTypeApplications
+MODE=${MODE:-"inplace"}
 
-$OR --mode inplace  $(find src -type f -name "*.hs" -exec grep -H -c 'CPP' {} \; | grep 0$ | cut -d':' -f1)
-$OR --mode inplace  $(find doc-tools -type f -name "*.hs" -exec grep -H -c 'CPP' {} \; | grep 0$ | cut -d':' -f1)
+$OR --mode=$MODE $(find src       -type f -name "*.hs" | cut -d':' -f1)
+$OR --mode=$MODE $(find doc-tools -type f -name "*.hs" | cut -d':' -f1)
